8e5b1b535b40e1330e7fefc895779298a42376b5
yoshi <yoshitakaOkada0214@gmail.com>
2021-03-29 21:32:41 +0900

    debug true

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wk$)s6c6ss_2&=f-k4h3#hzbtmcsi_&gqsm2trhqq#ts*l1+5#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['www.henojiya.net', '127.0.0.1', 'localhost']
 
