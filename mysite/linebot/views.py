59ece7e875b233903e5e6185cf11a3570b5096cb
yoshi <yoshitakaOkada0214@gmail.com>
2022-02-09 00:06:37 +0900

    linebot apiコードを .env に移行

--- mysite/linebot/views.py
+++ mysite/linebot/views.py
@@ -1,20 +1,13 @@
 """views.py"""
-import urllib.request
 import json
-from django.http import HttpResponseBadRequest, HttpResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render, redirect
-from django.http.response import JsonResponse
-from django.conf import settings
 from .models import LinePush
 
-# read accesstoken
-with open(settings.BASE_DIR.joinpath('linebot/api_setting/accesstoken.txt'), mode='r') as file:
-    ACCESSTOKEN = file.read()
 
 @csrf_exempt
 def callback(request):
-    """ラインの友達追加時に呼び出され、ラインのIDを登録する。"""
+    """ラインの友達追加時に呼び出され、ラインのIDを登録する"""
     if request.method == 'POST':
         request_json = json.loads(request.body.decode('utf-8'))
         events = request_json['events']
