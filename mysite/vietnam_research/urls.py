158056c64779c4f1fca273a72f91417ae7dc8dc5
yoshi <yoshitakaOkada0214@gmail.com>
2023-01-08 12:56:03 +0900

    refactoring

--- mysite/vietnam_research/urls.py
+++ mysite/vietnam_research/urls.py
@@ -1,15 +1,16 @@
 """docstring"""
 from django.urls import path
-from . import views
+from .views import ArticleCreateView, likes, index, WatchListRegister, WatchListEdit, FinancialResultsListView, \
+    FinancialResultsDetailListView, FinancialResultsCreateView
 
 app_name = 'vnm'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('likes/<int:user_id>/<int:article_id>/', views.likes, name='likes'),
-    path('article/create/', views.ArticleCreateView.as_view(), name="article_create"),
-    path('watchlist/register/', views.WatchListRegister.as_view(), name="watchlist_register"),
-    path('watchlist/edit/<int:pk>/', views.WatchListEdit.as_view(), name="watchlist_edit"),
-    path('financial_results/', views.FinancialResultsListView.as_view(), name="financial_results"),
-    path('financial_results/detail/<str:ticker>/', views.FinancialResultsDetailListView.as_view(), name="financial_results_detail"),
-    path('financial_results/create/', views.FinancialResultsCreateView.as_view(), name="financial_results_create")
+    path('', index, name='index'),
+    path('likes/<int:user_id>/<int:article_id>/', likes, name='likes'),
+    path('article/create/', ArticleCreateView.as_view(), name="article_create"),
+    path('watchlist/register/', WatchListRegister.as_view(), name="watchlist_register"),
+    path('watchlist/edit/<int:pk>/', WatchListEdit.as_view(), name="watchlist_edit"),
+    path('financial_results/', FinancialResultsListView.as_view(), name="financial_results"),
+    path('financial_results/detail/<str:ticker>/', FinancialResultsDetailListView.as_view(), name="financial_results_detail"),
+    path('financial_results/create/', FinancialResultsCreateView.as_view(), name="financial_results_create")
 ]
