3d762a7095edaeeb50e06df78e19dabfd3b7b68b
yoshi <yoshitakaOkada0214@gmail.com>
2023-01-15 12:16:43 +0900

    test通過

--- mysite/vietnam_research/tests/test_market_vietnam.py
+++ mysite/vietnam_research/tests/test_market_vietnam.py
@@ -7,5 +7,5 @@ class TestMarketVietnam(TestCase):
     def test_calc_fee(self):
         # TODO: なぜかユニットテストで失敗する（modelのimportがエラーを起こしている）
         mkt = MarketVietnam()
-        self.assertEqual(1533000, mkt.calc_fee(1500000))  # 1,500,000 VND の手数料は 1,533,000 VND
-        self.assertEqual(1200000, mkt.calc_fee(1150000))  # 1,200,000 VNDを下回ったときの手数料は 1,200,000 VND
+        self.assertEqual(1210000, mkt.calc_fee(55000000))  # 55,000,000 VND の手数料は 1,210,000 VND
+        self.assertEqual(1200000, mkt.calc_fee(50000000))  # 手数料が 1,200,000 VNDを下回ったときの手数料は 1,200,000 VND
