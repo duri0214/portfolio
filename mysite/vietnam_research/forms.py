d326f571c5b12630a15d1ceaad7e86318d9a9c87
yoshi <yoshitakaOkada0214@gmail.com>
2023-01-15 10:03:29 +0900

    FinancialResultsForm スキーマ変更に伴う修正

--- mysite/vietnam_research/forms.py
+++ mysite/vietnam_research/forms.py
@@ -43,4 +43,4 @@ class FinancialResultsForm(forms.ModelForm):
 
     class Meta:
         model = FinancialResultWatch
-        fields = ('date', 'ticker', 'quarter', 'eps_ok', 'sales_ok', 'guidance_ok', 'eps_unit', 'eps_estimate', 'eps_actual', 'sales_unit', 'sales_estimate', 'sales_actual', 'y_over_y_growth_rate', 'note_url')
+        fields = ('recorded_date', 'symbol', 'quarter', 'eps_ok', 'sales_ok', 'guidance_ok', 'eps_unit', 'eps_estimate', 'eps_actual', 'sales_unit', 'sales_estimate', 'sales_actual', 'y_over_y_growth_rate', 'note_url')
