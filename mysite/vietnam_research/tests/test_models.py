0e8b6bb7fd7590bb541c6e89d99667f317a4cd3d
yoshi <yoshitakaOkada0214@gmail.com>
2023-01-22 17:33:52 +0900

    テストクリア

--- mysite/vietnam_research/tests/test_models.py
+++ mysite/vietnam_research/tests/test_models.py
@@ -1,11 +1,14 @@
 import datetime
 from unittest import TestCase
 
-from mysite.vietnam_research.models import Industry
+from vietnam_research.models import Industry
 
 
 class TestIndustry(TestCase):
+    """
+    Notes: PyCharmのunittest経由だと失敗します（Requested setting INSTALLED_APPS ...）
+    `python manage.py test vietnam_research` でテストできます
+    """
     def test_slipped_month_end(self):
-        # TODO: なぜかユニットテストで失敗する（modelのimportがエラーを起こしている？）
-        expected_value = datetime.datetime(2022, 2, 28)
-        self.assertEqual(Industry.objects.slipped_month_end(-1, datetime.datetime(2022, 3, 15)), expected_value)
+        expected_value = datetime.date(2022, 2, 28)
+        self.assertEqual(Industry.objects.slipped_month_end(-1, datetime.datetime(2022, 3, 15)).recorded_date, expected_value)
