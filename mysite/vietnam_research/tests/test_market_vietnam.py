1d1dff8de826cfd5ca98cf4671ae6b19e7b5433e
yoshi <yoshitakaOkada0214@gmail.com>
2023-01-22 23:56:46 +0900

    テスト通過

--- mysite/vietnam_research/tests/test_market_vietnam.py
+++ mysite/vietnam_research/tests/test_market_vietnam.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
 
-from mysite.vietnam_research.service.market_vietnam import MarketVietnam
+from vietnam_research.service.market_vietnam import MarketVietnam
 
 
 class TestMarketVietnam(TestCase):
+    """
+    Notes: PyCharmのunittest経由だと失敗します（Requested setting INSTALLED_APPS ...）
+    `python manage.py test vietnam_research` でテストできます
+    """
     def test_calc_fee(self):
-        # TODO: なぜかユニットテストで失敗する（modelのimportがエラーを起こしている）
         mkt = MarketVietnam()
         self.assertEqual(1210000, mkt.calc_fee(55000000))  # 55,000,000 VND の手数料は 1,210,000 VND
         self.assertEqual(1200000, mkt.calc_fee(50000000))  # 手数料が 1,200,000 VNDを下回ったときの手数料は 1,200,000 VND
