b67713033e68e5d719eec800121778b8922485f3
yoshi <yoshitakaOkada0214@gmail.com>
2023-02-03 18:12:25 +0900

    テスト通過

--- mysite/vietnam_research/tests/management/test_daily_industry_uptrends.py
+++ mysite/vietnam_research/tests/management/test_daily_industry_uptrends.py
@@ -1,5 +1,3 @@
-from io import StringIO
-from django.core.management import call_command
 from django.test import TestCase
 import pandas as pd
 
@@ -12,9 +10,6 @@ class Test(TestCase):
     `python manage.py test --pattern="test_d*.py"` でテストできます
     """
     def test_calc_price(self):
-        # TODO: Django式のテストの仕方でやって
-        # See Also: https://docs.djangoproject.com/en/4.2/topics/testing/tools/#topics-testing-management-commands
-        call_command('daily_industry_uptrends', stdout=StringIO())
         data = pd.Series([7.17, 7.14, 7.07, 7.07, 7.19, 7.19, 7.12, 7.16, 7.40, 7.58, 7.59, 7.79, 8.33, 8.22])
         expected_value = 1.05
         self.assertEqual(calc_price(data)['delta'], expected_value)
