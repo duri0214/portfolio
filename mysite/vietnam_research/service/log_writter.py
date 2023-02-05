59d511a88d0a9a7456bdab138d2b1bef2810b77d
yoshi <yoshitakaOkada0214@gmail.com>
2023-02-05 22:01:35 +0900

    通常ログを実装

--- mysite/vietnam_research/service/log_writter.py
+++ mysite/vietnam_research/service/log_writter.py
@@ -16,7 +16,15 @@ def batch_is_done(number_of_records: int = 0, log_file_name: str = 'result.log')
     caller_dir = os.path.dirname(inspect.stack()[1].filename)
     caller_file_name = os.path.basename(inspect.stack()[1].filename)
     formatted_timestamp = localtime(now()).strftime('%Y-%m-%d %a %H:%M:%S')
-    formatted_number = f"({number_of_records})" if number_of_records > 0 else ""
+    formatted_number = f"({number_of_records})" if number_of_records > 0 else ''
     formatted_info = f"{formatted_timestamp} {caller_file_name} is done.{formatted_number}\n"
     with open(Path(caller_dir) / f"{log_file_name}", mode='a') as f:
+        f.write(formatted_info)
+
+
+def batch_information(text: str, log_file_name: str = 'result.log'):
+    caller_dir = os.path.dirname(inspect.stack()[1].filename)
+    formatted_timestamp = localtime(now()).strftime('%Y-%m-%d %a %H:%M:%S')
+    formatted_info = f"{formatted_timestamp} {text}\n"
+    with open(Path(caller_dir) / f"{log_file_name}", mode='a', encoding='utf8') as f:
         f.write(formatted_info)
