19e03110fc7fa71fb589b55d0a55538264d273b4
yoshi <yoshitakaOkada0214@gmail.com>
2023-02-19 15:10:37 +0900

    ubuntuで事故るので修正。不要

--- mysite/vietnam_research/service/market_abstract.py
+++ mysite/vietnam_research/service/market_abstract.py
@@ -1,14 +1,10 @@
 from abc import ABCMeta, abstractmethod
 
 from django.db.models import QuerySet
-from sqlalchemy import create_engine
 
 
 class MarketAbstract(metaclass=ABCMeta):
     """各マーケットのための基底クラス"""
-    _con_str = 'mysql+mysqldb://python:python123@127.0.0.1/pythondb?charset=utf8&use_unicode=1'
-    _con = create_engine(_con_str, echo=False).connect()
-
     @abstractmethod
     def watchlist(self) -> QuerySet:
         raise NotImplementedError()
