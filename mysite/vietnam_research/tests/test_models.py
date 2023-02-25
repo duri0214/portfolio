2b952e560fb25800d38102c0b69b58318d6aab53
yoshi <yoshitakaOkada0214@gmail.com>
2023-02-25 22:59:28 +0900

    test fixed

--- mysite/vietnam_research/tests/test_models.py
+++ mysite/vietnam_research/tests/test_models.py
@@ -24,10 +24,16 @@ class TestIndustry(TestCase):
     def test_record_count_is_one(self):
         Industry.objects.create(
             recorded_date='2022-03-01',
-            market=Market.objects.get(code='HOSE'),
-            symbol=Symbol.objects.get(code='AAA'),
-            ind_class=IndClass.objects.get(industry1='農林水産業', industry2='天然ゴム'),
-            created_at=now()
+            open_price=12.4,
+            high_price=12.65,
+            low_price=12.25,
+            closing_price=12.5,
+            volume=2037710.0,
+            trade_price_of_a_day=25382060,
+            marketcap=100.0,
+            per=6.76,
+            created_at=now(),
+            symbol=Symbol.objects.get(code='AAA')
         )
         self.assertEqual(Industry.objects.all().count(), 1)
 
@@ -37,23 +43,35 @@ class TestIndustry(TestCase):
         ind_class = IndClass.objects.get(industry1='農林水産業', industry2='天然ゴム')
         Industry.objects.create(
             recorded_date='2023-02-02',
-            market=market,
-            symbol=symbol,
-            ind_class=ind_class,
-            created_at=now()
+            open_price=12.4,
+            high_price=12.65,
+            low_price=12.25,
+            closing_price=12.5,
+            volume=2037710.0,
+            trade_price_of_a_day=25382060,
+            marketcap=100.0,
+            per=6.76,
+            created_at=now(),
+            symbol=Symbol.objects.get(code='AAA')
         )
         industry = Industry.objects.filter(recorded_date='2023-02-02').get(symbol__code='AAA')
-        self.assertEqual(industry.market, market)
+        self.assertEqual(industry.symbol.market, market)
         self.assertEqual(industry.symbol, symbol)
-        self.assertEqual(industry.ind_class, ind_class)
+        self.assertEqual(industry.symbol.ind_class, ind_class)
 
     def test_slipped_month_end(self):
         Industry.objects.create(
             recorded_date='2022-02-28',
-            market=Market.objects.get(code='HOSE'),
-            symbol=Symbol.objects.get(code='AAA'),
-            ind_class=IndClass.objects.get(industry1='農林水産業', industry2='天然ゴム'),
-            created_at=now()
+            open_price=12.4,
+            high_price=12.65,
+            low_price=12.25,
+            closing_price=12.5,
+            volume=2037710.0,
+            trade_price_of_a_day=25382060,
+            marketcap=100.0,
+            per=6.76,
+            created_at=now(),
+            symbol=Symbol.objects.get(code='AAA')
         )
         expected_value = datetime.date(2022, 2, 28)
         self.assertEqual(Industry.objects.slipped_month_end(-1, datetime.datetime(2022, 3, 15)).recorded_date, expected_value)
