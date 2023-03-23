0f43a119728b34ee029a5118dddb84e53d8deb77
yoshi <yoshitakaOkada0214@gmail.com>
2023-03-23 22:39:25 +0900

    型を明示

--- mysite/taxonomy/domain/breed_entity.py
+++ mysite/taxonomy/domain/breed_entity.py
@@ -3,16 +3,16 @@ class BreedEntity:
     kingdom界 - phylum門 - classification綱 - family科 - genus属 - species腫 - breed品種
     """
     def __init__(self, record: dict):
-        self.kingdom = record.get('kingdom_name')
-        self.phylum = record.get('phylum_name')
-        self.classification = record.get('classification_name')
-        self.family = record.get('family_name')
-        self.genus = record.get('genus_name')
-        self.species = record.get('species_name')
-        self.breed = record.get('breed_name')
-        self.breed_kana = record.get('breed_name_kana')
-        self.natural_monument = record.get('breed_name_kana')
-        self.breed_tag = record.get('breed_tag')
+        self.kingdom: str = record.get('kingdom_name')
+        self.phylum: str = record.get('phylum_name')
+        self.classification: str = record.get('classification_name')
+        self.family: str = record.get('family_name')
+        self.genus: str = record.get('genus_name')
+        self.species: str = record.get('species_name')
+        self.breed: str = record.get('breed_name')
+        self.breed_kana: str = record.get('breed_name_kana')
+        self.natural_monument: str = record.get('breed_name_kana')
+        self.breed_tag: str = record.get('breed_tag')
 
     def get_hierarchy(self) -> tuple:
         """
