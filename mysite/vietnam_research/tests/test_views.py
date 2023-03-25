8f26df62d338911768d566ff21541eda85769cae
yoshi <yoshitakaOkada0214@gmail.com>
2023-03-25 15:55:16 +0900

    test ok

--- mysite/vietnam_research/tests/test_views.py
+++ mysite/vietnam_research/tests/test_views.py
@@ -7,10 +7,17 @@ from register.models import User
 class TestView(TestCase):
     @classmethod
     def setUpTestData(cls):
-        User.objects.create_user(email='tester@b.c').set_password('12345')
+        tester = User.objects.create_user(email='tester@b.c')
+        tester.set_password('12345')
+        cls.tester = tester
+        cls.article = Articles.objects.create(title='Hello', note='How are you', user=tester)
 
-    def test_login_and_logout(self):
-        user = User.objects.get(email='tester@b.c')
+    def test_status_code_200(self):
+        client = Client()
+        response = client.get(reverse('vnm:index'))
+        self.assertEqual(200, response.status_code)
+
+    def test_login_as_guest(self):
         client = Client()
         response = client.get(reverse('vnm:index'))
         self.assertContains(response, 'ゲストさん')
