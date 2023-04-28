c710b0f4fc6eca2cca3e6293ba523b6dc844bcdf
yoshi <yoshitakaOkada0214@gmail.com>
2023-04-28 23:06:47 +0900

    sqlalchemyがらみのテストを無効化

--- mysite/vietnam_research/tests/service/test_market_nasdaq.py
+++ mysite/vietnam_research/tests/service/test_market_nasdaq.py
@@ -5,6 +5,8 @@ from vietnam_research.service.market_nasdaq import MarketNasdaq
 
 class TestMarketNasdaq(TestCase):
     def test_calc_fee(self):
-        mkt = MarketNasdaq()
-        self.assertEqual(22, mkt.calc_fee(5000))    # $5000 の手数料は $24.75->$22
-        self.assertEqual(2.475, mkt.calc_fee(500))  # $500 の手数料は  $2.475
+        # TODO: sqlalchemyをアンインストールするほうを優先したので、テストはいったんOFF
+        # mkt = MarketNasdaq()
+        # self.assertEqual(22, mkt.calc_fee(5000))    # $5000 の手数料は $24.75->$22
+        # self.assertEqual(2.475, mkt.calc_fee(500))  # $500 の手数料は  $2.475
+        self.fail()
