0ee403806218a850c1e734763fbf365e59799439
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-08 23:57:55 +0900

    refactoring: get_watchlist, vnindex_timeline

--- vietnam_research/domain/repository/marketrepository.py
+++ vietnam_research/domain/repository/marketrepository.py
@@ -1,6 +1,9 @@
-from django.db.models import F
+from django.db.models import F, Max
+from django.db.models import QuerySet
+from django.db.models.functions import Round
 
-from vietnam_research.models import Articles, BasicInformation
+from vietnam_research.models import Articles, BasicInformation, VnIndex
+from vietnam_research.models import Industry, Watchlist
 
 
 class MarketRepository:
@@ -16,3 +19,57 @@ class MarketRepository:
     @staticmethod
     def get_basic_info():
         return BasicInformation.objects.order_by("id").values("item", "description")
+
+    @staticmethod
+    def get_watchlist():
+        """
+        ウォッチリストを作成するためのクエリセットを作成します。
+
+        closing_price: 終値は1,000VND単位なので、表示上は1,000を掛けている（1VND単位で表示）
+        stocks_price_yen: VND→JPNへの変換は 200VND ≒ 1JPY
+        buy_price_yen: 当初購入額（単価×購入株数）
+        stocks_price_delta: 直近終値÷当初購入単価
+
+        Returns:
+            QuerySet: Watchlistをベースに換算額などの計算を組み合わせたもの
+        """
+        latest_date = Industry.objects.aggregate(Max("recorded_date"))[
+            "recorded_date__max"
+        ]
+
+        return (
+            Watchlist.objects.filter(already_has=1)
+            .filter(symbol__industry__recorded_date=latest_date)
+            .annotate(closing_price=Round(F("symbol__industry__closing_price") * 1000))
+            .annotate(stocks_price_yen=F("stocks_price") / 200)
+            .annotate(buy_price_yen=F("stocks_price_yen") * F("stocks_count"))
+            .annotate(
+                stocks_price_delta=Round(
+                    (F("closing_price") / F("stocks_price") - 1) * 100, 2
+                )
+            )
+        )
+
+    @staticmethod
+    def get_vnindex_timeline() -> dict:
+        """
+        vn-indexのシンプルなYM時系列データセットを作成します
+
+        See Also: https://www.chartjs.org/docs/latest/getting-started/
+        """
+        records = VnIndex.objects.time_series_closing_price()
+        vnindex_timeline = {
+            "labels": [
+                record["Y"] + record["M"] for record in records.order_by("Y", "M")
+            ],
+            "datasets": [
+                {
+                    "label": "VN-Index",
+                    "data": [
+                        record["closing_price"] for record in records.order_by("Y", "M")
+                    ],
+                }
+            ],
+        }
+
+        return vnindex_timeline
--- vietnam_research/domain/service/marketservice.py
+++ vietnam_research/domain/service/marketservice.py
@@ -4,13 +4,13 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 from django.db.models import QuerySet
-from django.db.models import Sum, F, Value, Count, CharField, FloatField, Max
+from django.db.models import Sum, F, Value, Count, CharField, FloatField
 from django.db.models.functions import Concat, Round
 
 from config.settings import STATIC_ROOT
 from vietnam_research.domain.repository.marketrepository import MarketRepository
 from vietnam_research.forms import ExchangeForm
-from vietnam_research.models import Industry, Watchlist, VnIndex, Uptrends
+from vietnam_research.models import Industry, VnIndex, Uptrends
 
 
 class MarketAbstract(ABC):
@@ -40,10 +40,6 @@ class NasdaqMarketDataProvider(MarketAbstract):
 
 
 class VietnamMarketDataProvider(MarketAbstract):
-    """
-    ベトナムのマーケットを処理します
-    """
-
     def sbi_topics(self, filename: str = "market_report_fo_em_topic.txt"):
         """
         バッチ（daily_sbi_topics.py download_pdf）で取り込んで決まった場所においたtxtを読み込んで返す\n
@@ -67,55 +63,19 @@ class VietnamMarketDataProvider(MarketAbstract):
         """
         ウォッチリストを作成します
 
-        closing_price: 終値は1,000VND単位なので、表示上は1,000を掛けている（1VND単位で表示）\n
-        stocks_price_yen: VND→JPNへの変換は 200VND ≒ 1JPY\n
-        buy_price_yen: 当初購入額（単価×購入株数）\n
-        stocks_price_delta: 直近終値÷当初購入単価
-
         Returns:
             QuerySet: Watchlistをベースに換算額などの計算を組み合わせたもの
         """
-        latest_date = Industry.objects.aggregate(Max("recorded_date"))[
-            "recorded_date__max"
-        ]
+        return MarketRepository.get_watchlist()
 
-        return (
-            Watchlist.objects.filter(already_has=1)
-            .filter(symbol__industry__recorded_date=latest_date)
-            .annotate(closing_price=Round(F("symbol__industry__closing_price") * 1000))
-            .annotate(stocks_price_yen=F("stocks_price") / 200)
-            .annotate(buy_price_yen=F("stocks_price_yen") * F("stocks_count"))
-            .annotate(
-                stocks_price_delta=Round(
-                    (F("closing_price") / F("stocks_price") - 1) * 100, 2
-                )
-            )
-        )
-
-    @staticmethod
-    def vnindex_timeline() -> dict:
+    def vnindex_timeline(self) -> dict:
         """
         vn-indexのシンプルなYM時系列データセットを作成します
 
-        See Also: https://www.chartjs.org/docs/latest/getting-started/
+        Returns:
+            dict: VN-Indexのタイムラインデータ
         """
-        records = VnIndex.objects.time_series_closing_price()
-        vnindex_timeline = {
-            "labels": [
-                record["Y"] + record["M"] for record in records.order_by("Y", "M")
-            ],
-            "datasets": [
-                {
-                    "label": "VN-Index",
-                    "data": [
-                        record["closing_price"] for record in records.order_by("Y", "M")
-                    ],
-                }
-            ],
-        }
-        # print('\nvnindex_timeline: ', vnindex_timeline)
-
-        return vnindex_timeline
+        return MarketRepository.get_vnindex_timeline()
 
     @staticmethod
     def vnindex_annual_layers() -> dict:
