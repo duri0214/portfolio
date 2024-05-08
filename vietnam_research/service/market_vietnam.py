03f8e423794cb7fe0b54a7145e729082051809dc
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-08 23:04:19 +0900

    fix: abstractとnasdaqのファイル統合

--- vietnam_research/service/market_vietnam.py
+++ vietnam_research/service/market_vietnam.py
@@ -1,12 +1,42 @@
 import logging
+from abc import ABCMeta, abstractmethod
 from pathlib import Path
 
-from django.db.models import Sum, F, QuerySet, Value, Count, CharField, FloatField, Max
+from django.db.models import QuerySet
+from django.db.models import Sum, F, Value, Count, CharField, FloatField, Max
 from django.db.models.functions import Concat, Round
 
 from config.settings import STATIC_ROOT
-from vietnam_research.service.market_abstract import MarketAbstract
 from vietnam_research.models import Industry, Watchlist, VnIndex, Uptrends
+
+
+class MarketAbstract(metaclass=ABCMeta):
+    """各マーケットのための基底クラス"""
+
+    @abstractmethod
+    def watchlist(self) -> QuerySet:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def sbi_topics(self) -> str:
+        raise NotImplementedError()
+
+    @staticmethod
+    @abstractmethod
+    def calc_fee(price_without_fees: float) -> float:
+        raise NotImplementedError()
+
+
+class MarketNasdaq(MarketAbstract):
+    def watchlist(self) -> QuerySet:
+        pass
+
+    def sbi_topics(self) -> str:
+        pass
+
+    @staticmethod
+    def calc_fee(price_without_fees: float) -> float:
+        pass
 
 
 class MarketVietnam(MarketAbstract):
@@ -24,7 +54,9 @@ class MarketVietnam(MarketAbstract):
 
         See Also: https://search.sbisec.co.jp/v2/popwin/info/stock/market_report_fo_em_topic.pdf
         """
-        filepath = STATIC_ROOT / Path('vietnam_research/sbi_topics/market_report_fo_em_topic.txt')
+        filepath = STATIC_ROOT / Path(
+            "vietnam_research/sbi_topics/market_report_fo_em_topic.txt"
+        )
         try:
             with open(filepath, encoding="utf8") as f:
                 sbi_topics = f.read()
@@ -45,15 +77,22 @@ class MarketVietnam(MarketAbstract):
         Returns:
             QuerySet: Watchlistをベースに換算額などの計算を組み合わせたもの
         """
-        latest_date = Industry.objects.aggregate(Max('recorded_date'))['recorded_date__max']
+        latest_date = Industry.objects.aggregate(Max("recorded_date"))[
+            "recorded_date__max"
+        ]
 
-        return Watchlist.objects \
-            .filter(already_has=1) \
-            .filter(symbol__industry__recorded_date=latest_date) \
-            .annotate(closing_price=Round(F('symbol__industry__closing_price') * 1000)) \
-            .annotate(stocks_price_yen=F('stocks_price') / 200) \
-            .annotate(buy_price_yen=F('stocks_price_yen') * F('stocks_count')) \
-            .annotate(stocks_price_delta=Round((F('closing_price') / F('stocks_price') - 1) * 100, 2))
+        return (
+            Watchlist.objects.filter(already_has=1)
+            .filter(symbol__industry__recorded_date=latest_date)
+            .annotate(closing_price=Round(F("symbol__industry__closing_price") * 1000))
+            .annotate(stocks_price_yen=F("stocks_price") / 200)
+            .annotate(buy_price_yen=F("stocks_price_yen") * F("stocks_count"))
+            .annotate(
+                stocks_price_delta=Round(
+                    (F("closing_price") / F("stocks_price") - 1) * 100, 2
+                )
+            )
+        )
 
     @staticmethod
     def vnindex_timeline() -> dict:
@@ -64,11 +103,17 @@ class MarketVietnam(MarketAbstract):
         """
         records = VnIndex.objects.time_series_closing_price()
         vnindex_timeline = {
-            "labels": [record['Y'] + record['M'] for record in records.order_by('Y', 'M')],
-            "datasets": [{
-                "label": 'VN-Index',
-                "data": [record['closing_price'] for record in records.order_by('Y', 'M')]
-            }]
+            "labels": [
+                record["Y"] + record["M"] for record in records.order_by("Y", "M")
+            ],
+            "datasets": [
+                {
+                    "label": "VN-Index",
+                    "data": [
+                        record["closing_price"] for record in records.order_by("Y", "M")
+                    ],
+                }
+            ],
         }
         # print('\nvnindex_timeline: ', vnindex_timeline)
 
@@ -83,12 +128,21 @@ class MarketVietnam(MarketAbstract):
         """
         records = VnIndex.objects.time_series_closing_price()
         vnindex_layers = {
-            "labels": [record['M'] for record in records.values('M').distinct().order_by('M')],
-            "datasets": []
+            "labels": [
+                record["M"] for record in records.values("M").distinct().order_by("M")
+            ],
+            "datasets": [],
         }
-        for year in [record['Y'] for record in records.values('Y').distinct().order_by('Y')]:
-            a_year_records = records.filter(Y=year).order_by('Y', 'M').values('closing_price')
-            inner = {"label": year, "data": [record['closing_price'] for record in a_year_records]}
+        for year in [
+            record["Y"] for record in records.values("Y").distinct().order_by("Y")
+        ]:
+            a_year_records = (
+                records.filter(Y=year).order_by("Y", "M").values("closing_price")
+            )
+            inner = {
+                "label": year,
+                "data": [record["closing_price"] for record in a_year_records],
+            }
             vnindex_layers["datasets"].append(inner)
         # print('\nvnindex_layers: ', vnindex_layers)
 
@@ -136,30 +190,46 @@ class MarketVietnam(MarketAbstract):
         result = []
         for m in months_dating_back:
             try:
-                lastday_of_the_month = Industry.objects.slipped_month_end(m).formatted_recorded_date()
+                end_of_month = Industry.objects.slipped_month_end(
+                    m
+                ).formatted_recorded_date()
             except Industry.DoesNotExist:
-                logging.warning(f"market_vietnam.py radar_chart_count() の{m}ヶ月は存在しないため、無視されました")
+                logging.warning(
+                    f"market_vietnam.py radar_chart_count() の{m}ヶ月は存在しないため、無視されました"
+                )
                 continue
-            denominator = len(Industry.objects.filter(recorded_date=lastday_of_the_month))
-            industry_records = Industry.objects \
-                .filter(recorded_date=lastday_of_the_month) \
-                .annotate(ind_name=Concat(F('symbol__ind_class__industry_class'), Value('|'),
-                                          F('symbol__ind_class__industry1'), output_field=CharField())) \
-                .values('ind_name') \
-                .annotate(count=Count('id')) \
-                .annotate(cnt_per=Round(F('count') / denominator * 100, precision=2, output_field=FloatField())) \
-                .order_by('ind_name')
+            denominator = len(Industry.objects.filter(recorded_date=end_of_month))
+            industry_records = (
+                Industry.objects.filter(recorded_date=end_of_month)
+                .annotate(
+                    ind_name=Concat(
+                        F("symbol__ind_class__industry_class"),
+                        Value("|"),
+                        F("symbol__ind_class__industry1"),
+                        output_field=CharField(),
+                    )
+                )
+                .values("ind_name")
+                .annotate(count=Count("id"))
+                .annotate(
+                    cnt_per=Round(
+                        F("count") / denominator * 100,
+                        precision=2,
+                        output_field=FloatField(),
+                    )
+                )
+                .order_by("ind_name")
+            )
             inner = []
             # print("industry_records(sql): ", industry_records.query)
             for industry_record in industry_records:
-                inner.append({
-                    "axis": industry_record["ind_name"],
-                    "value": industry_record["cnt_per"]
-                })
-            result.append({
-                "name": f"企業数 {m}ヶ月前",
-                "axes": inner
-            })
+                inner.append(
+                    {
+                        "axis": industry_record["ind_name"],
+                        "value": industry_record["cnt_per"],
+                    }
+                )
+            result.append({"name": f"企業数 {m}ヶ月前", "axes": inner})
 
         return result
 
@@ -182,61 +252,100 @@ class MarketVietnam(MarketAbstract):
         result = []
         for m in months_dating_back:
             try:
-                lastday_of_the_month = Industry.objects.slipped_month_end(m).formatted_recorded_date()
+                end_of_month = Industry.objects.slipped_month_end(
+                    m
+                ).formatted_recorded_date()
             except Industry.DoesNotExist:
-                logging.warning(f"market_vietnam.py radar_chart_count() の{m}ヶ月は存在しないため、無視されました")
+                logging.warning(
+                    f"market_vietnam.py radar_chart_count() の{m}ヶ月は存在しないため、無視されました"
+                )
                 continue
-            records = Industry.objects.filter(recorded_date=lastday_of_the_month).values('marketcap')
+            records = Industry.objects.filter(recorded_date=end_of_month).values(
+                "marketcap"
+            )
             denominator = sum([record["marketcap"] for record in records])
-            industry_records = Industry.objects \
-                .filter(recorded_date=lastday_of_the_month) \
-                .annotate(ind_name=Concat(F('symbol__ind_class__industry_class'), Value('|'),
-                                          F('symbol__ind_class__industry1'), output_field=CharField())) \
-                .values('ind_name') \
-                .annotate(marketcap_sum=Sum('marketcap')) \
-                .annotate(cap_per=Round(F('marketcap_sum') / denominator * 100, precision=2,
-                                        output_field=FloatField())) \
-                .order_by('ind_name')
+            industry_records = (
+                Industry.objects.filter(recorded_date=end_of_month)
+                .annotate(
+                    ind_name=Concat(
+                        F("symbol__ind_class__industry_class"),
+                        Value("|"),
+                        F("symbol__ind_class__industry1"),
+                        output_field=CharField(),
+                    )
+                )
+                .values("ind_name")
+                .annotate(marketcap_sum=Sum("marketcap"))
+                .annotate(
+                    cap_per=Round(
+                        F("marketcap_sum") / denominator * 100,
+                        precision=2,
+                        output_field=FloatField(),
+                    )
+                )
+                .order_by("ind_name")
+            )
             # print("industry_records(sql): ", industry_records.query)
             inner = []
             for industry_record in industry_records:
-                inner.append({
-                    "axis": industry_record["ind_name"],
-                    "value": industry_record["cap_per"]
-                })
-            result.append({
-                "name": f"時価総額 {m}ヶ月前",
-                "axes": inner
-            })
+                inner.append(
+                    {
+                        "axis": industry_record["ind_name"],
+                        "value": industry_record["cap_per"],
+                    }
+                )
+            result.append({"name": f"時価総額 {m}ヶ月前", "axes": inner})
 
         return result
 
     @staticmethod
     def uptrends() -> dict:
-        uptrends = Uptrends.objects.prefetch_related('symbol', 'ind_class') \
+        uptrends = (
+            Uptrends.objects.prefetch_related("symbol", "ind_class")
             .annotate(
-            industry1=F('symbol__ind_class__industry1'),
-            industry_class=F('symbol__ind_class__industry_class'),
-            ind_name=Concat(F('symbol__ind_class__industry_class'), Value('|'), F('symbol__ind_class__industry1'),
-                            output_field=CharField()),
-            url_file_name=F('symbol__market__url_file_name'),
-            code=F('symbol__code')
-        ) \
-            .order_by('symbol__ind_class__industry_class', 'symbol__ind_class__industry1', '-stocks_price_delta') \
-            .values('industry1', 'ind_name', 'code', 'url_file_name', 'stocks_price_latest', 'stocks_price_delta')
+                industry1=F("symbol__ind_class__industry1"),
+                industry_class=F("symbol__ind_class__industry_class"),
+                ind_name=Concat(
+                    F("symbol__ind_class__industry_class"),
+                    Value("|"),
+                    F("symbol__ind_class__industry1"),
+                    output_field=CharField(),
+                ),
+                url_file_name=F("symbol__market__url_file_name"),
+                code=F("symbol__code"),
+            )
+            .order_by(
+                "symbol__ind_class__industry_class",
+                "symbol__ind_class__industry1",
+                "-stocks_price_delta",
+            )
+            .values(
+                "industry1",
+                "ind_name",
+                "code",
+                "url_file_name",
+                "stocks_price_latest",
+                "stocks_price_delta",
+            )
+        )
 
         result = {}
-        ind_names = Uptrends.objects \
-            .annotate(
-            ind_name=Concat(F('symbol__ind_class__industry_class'), Value('|'), F('symbol__ind_class__industry1'),
-                            output_field=CharField())
-        ) \
-            .distinct() \
-            .order_by('ind_name') \
-            .values('ind_name')
+        ind_names = (
+            Uptrends.objects.annotate(
+                ind_name=Concat(
+                    F("symbol__ind_class__industry_class"),
+                    Value("|"),
+                    F("symbol__ind_class__industry1"),
+                    output_field=CharField(),
+                )
+            )
+            .distinct()
+            .order_by("ind_name")
+            .values("ind_name")
+        )
 
-        ind_names = [x['ind_name'] for x in list(ind_names)]
+        ind_names = [x["ind_name"] for x in list(ind_names)]
         for ind_name in ind_names:
-            result[ind_name] = [x for x in uptrends if x['ind_name'] == ind_name]
+            result[ind_name] = [x for x in uptrends if x["ind_name"] == ind_name]
 
         return result
