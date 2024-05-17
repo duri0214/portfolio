2976c9851961f64418ea69ac04c385e6fa899cd8
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-17 22:58:26 +0900

    fix: 署名を検証する

--- linebot/views.py
+++ linebot/views.py
@@ -1,4 +1,8 @@
+import base64
+import hashlib
+import hmac
 import json
+import os
 
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
@@ -13,13 +17,31 @@ WEBHOOK_VERIFICATION_USER_ID = "Udeadbeefdeadbeefdeadbeefdeadbeef"
 @method_decorator(csrf_exempt, name="dispatch")
 class CallbackView(View):
     @staticmethod
-    def post(request, *args, **kwargs):
+    def is_valid_signature(request):
+        body = request.body.decode("utf-8")
+        _hash = hmac.new(
+            os.environ.get("LINE_CHANNEL_SECRET").encode("utf-8"),
+            body.encode("utf-8"),
+            hashlib.sha256,
+        ).digest()
+        signature = base64.b64encode(_hash).decode("utf-8")
+
+        # Retrieve X-Line-Signature header value
+        x_line_signature = request.META.get("HTTP_X_LINE_SIGNATURE")
+
+        # Compare X-Line-Signature header value and the signature calculated in the code
+        return x_line_signature == signature
+
+    def post(self, request, *args, **kwargs):
         """
         ラインの友達追加時に呼び出され、ラインのIDを登録する
 
         Notes: LINE DEVELOPERSの画面からWebhookの接続をテストした場合
                実際のイベント（ユーザーのアクションなど）がないため、eventsデータは存在せず、空の配列が返される
         """
+        if not self.is_valid_signature(request):
+            return HttpResponse(status=403)  # return 'Forbidden'
+
         request_json = json.loads(request.body.decode("utf-8"))
         events = request_json["events"]
 
