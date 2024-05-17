d8d3fe716f942ad15000ff80745b049088b34c36
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-18 00:49:00 +0900

    fix: model増強

--- linebot_engine/models.py
+++ linebot_engine/models.py
@@ -1,10 +1,12 @@
-"""models.py"""
 from django.db import models
 
 
 class LinePush(models.Model):
     """Lineでのプッシュ先を表す"""
+
     user_id = models.CharField(max_length=100, unique=True)
+    display_name = models.CharField(max_length=100, null=True)
+    picture = models.ImageField(upload_to="linebot_engine/", null=True)
 
     def __str__(self):
         return self.user_id
