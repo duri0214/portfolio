2745a8330cbc11cd7d47c66c925668c500c9ba9e
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-18 18:44:10 +0900

    fix: ユニーク制約

--- vietnam_research/models.py
+++ vietnam_research/models.py
@@ -1,8 +1,8 @@
 import datetime
 
 from dateutil.relativedelta import relativedelta
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 from django.db.models import QuerySet, IntegerField, Case, Count, When
 
 
@@ -16,12 +16,13 @@ class Market(models.Model):
 
     See Also: https://www.viet-kabu.com/stock/hcm.html
     """
+
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=50)
     url_file_name = models.CharField(max_length=10, null=True)
 
     class Meta:
-        db_table = 'vietnam_research_m_market'
+        db_table = "vietnam_research_m_market"
 
 
 class IndClass(models.Model):
@@ -34,12 +35,14 @@ class IndClass(models.Model):
 
     See Also: https://www.viet-kabu.com/stock/hcm.html
     """
+
     industry1 = models.CharField(max_length=10)
     industry2 = models.CharField(max_length=20)
     industry_class = models.IntegerField()
 
     class Meta:
-        db_table = 'vietnam_research_m_industry_class'
+        db_table = "vietnam_research_m_industry_class"
+        unique_together = (("industry1", "industry2"),)
 
 
 class Symbol(models.Model):
@@ -48,17 +51,17 @@ class Symbol(models.Model):
 
     See Also: https://www.viet-kabu.com/stock/hcm.html
     """
+
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=255)
     market = models.ForeignKey(Market, on_delete=models.CASCADE)
     ind_class = models.ForeignKey(IndClass, on_delete=models.CASCADE, null=True)
 
     class Meta:
-        db_table = 'vietnam_research_m_symbol'
+        db_table = "vietnam_research_m_symbol"
         constraints = [
             models.UniqueConstraint(
-                fields=['code', 'market_id'],
-                name='code_market_id_unique'
+                fields=["code", "market_id"], name="code_market_id_unique"
             )
         ]
 
@@ -67,7 +70,9 @@ class Symbol(models.Model):
 
 
 class IndustryQuerySet(models.QuerySet):
-    def slipped_month_end(self, month_shift: int, base_date=datetime.datetime.today()) -> QuerySet:
+    def slipped_month_end(
+        self, month_shift: int, base_date=datetime.datetime.today()
+    ) -> QuerySet:
         """
         Xヶ月前の、（Industryにデータが存在する）月末日のレコードを取得する
 
@@ -80,10 +85,14 @@ class IndustryQuerySet(models.QuerySet):
         """
         slipped_date = base_date + relativedelta(months=month_shift, day=31)
 
-        return self \
-            .filter(recorded_date__year=slipped_date.year, recorded_date__month=slipped_date.month) \
-            .order_by('recorded_date') \
-            .latest('recorded_date')
+        return (
+            self.filter(
+                recorded_date__year=slipped_date.year,
+                recorded_date__month=slipped_date.month,
+            )
+            .order_by("recorded_date")
+            .latest("recorded_date")
+        )
 
 
 class Industry(models.Model):
@@ -97,6 +106,7 @@ class Industry(models.Model):
 
     See Also: https://www.viet-kabu.com/stock/hcm.html
     """
+
     recorded_date = models.DateField()
     symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
     open_price = models.FloatField()
@@ -111,7 +121,7 @@ class Industry(models.Model):
 
     objects = IndustryQuerySet.as_manager()
 
-    def formatted_recorded_date(self, format_at='%Y-%m-%d') -> str:
+    def formatted_recorded_date(self, format_at="%Y-%m-%d") -> str:
         """
         指定した書式で recorded_date を返す
 
@@ -126,7 +136,7 @@ class Industry(models.Model):
 
 class VnIndexQuerySet(models.QuerySet):
     def time_series_closing_price(self) -> QuerySet:
-        return self.order_by('Y', 'M').values('Y', 'M', 'closing_price').distinct()
+        return self.order_by("Y", "M").values("Y", "M", "closing_price").distinct()
 
 
 class VnIndex(models.Model):
@@ -135,6 +145,7 @@ class VnIndex(models.Model):
 
     See Also: https://jp.investing.com/indices/vn-historical-data
     """
+
     Y = models.CharField(max_length=4)
     M = models.CharField(max_length=2)
     closing_price = models.FloatField()
@@ -142,16 +153,12 @@ class VnIndex(models.Model):
     objects = VnIndexQuerySet.as_manager()
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['Y', 'M'],
-                name='y_m_unique'
-            )
-        ]
+        constraints = [models.UniqueConstraint(fields=["Y", "M"], name="y_m_unique")]
 
 
 class Watchlist(models.Model):
     """ウォッチリスト"""
+
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
     already_has = models.BooleanField(blank=True, null=True, default=1)
@@ -162,6 +169,7 @@ class Watchlist(models.Model):
 
 class Uptrends(models.Model):
     """日次バッチでUptrends（傾き計算考慮）を出します"""
+
     stocks_price_oldest = models.FloatField()
     stocks_price_latest = models.FloatField()
     stocks_price_delta = models.FloatField()
@@ -175,10 +183,11 @@ class Sbi(models.Model):
     See Also: https://search.sbisec.co.jp/v2/popwin/info/stock/pop6040_vn_list.html
     See Also: https://search.sbisec.co.jp/v2/popwin/info/stock/pop6040_usequity_list.html
     """
+
     symbol = models.ForeignKey(Symbol, on_delete=models.SET_NULL, null=True)
 
     class Meta:
-        db_table = 'vietnam_research_m_sbi'
+        db_table = "vietnam_research_m_sbi"
 
 
 class BasicInformation(models.Model):
@@ -187,54 +196,59 @@ class BasicInformation(models.Model):
 
     See Also: https://www.jetro.go.jp/world/asia/vn/basic_01.html
     """
+
     item = models.TextField()
     description = models.TextField(blank=True, null=True)
 
 
 class Articles(models.Model):
     """いいね！機能つきの記事"""
-    title = models.CharField(verbose_name='タイトル', max_length=200)
-    note = models.TextField(verbose_name='投稿内容')
+
+    title = models.CharField(verbose_name="タイトル", max_length=200)
+    note = models.TextField(verbose_name="投稿内容")
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    created_at = models.DateTimeField('公開日時', auto_now_add=True)
+    created_at = models.DateTimeField("公開日時", auto_now_add=True)
 
     @staticmethod
     def with_state(user_id: int) -> QuerySet:
         return Articles.objects.annotate(
-            likes_cnt=Count('likes'),
+            likes_cnt=Count("likes"),
             liked_by_me=Case(
-                When(id__in=Likes.objects.filter(user_id=user_id).values('articles_id'),
-                     then=1),
+                When(
+                    id__in=Likes.objects.filter(user_id=user_id).values("articles_id"),
+                    then=1,
+                ),
                 default=0,
-                output_field=IntegerField()
-            )
+                output_field=IntegerField(),
+            ),
         )
 
 
 class Likes(models.Model):
     """いいね"""
-    articles = models.ForeignKey('Articles', on_delete=models.CASCADE)
+
+    articles = models.ForeignKey("Articles", on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['articles_id', 'user_id'],
-                name='articles_user_unique'
+                fields=["articles_id", "user_id"], name="articles_user_unique"
             )
         ]
 
 
 class Unit(models.Model):
     """財務単位"""
+
     name = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        db_table = 'vietnam_research_m_unit'
+        db_table = "vietnam_research_m_unit"
 
 
 class FinancialResultWatch(models.Model):
@@ -243,6 +257,7 @@ class FinancialResultWatch(models.Model):
 
     recorded_date: 計上日
     """
+
     recorded_date = models.DateField()
     quarter = models.SmallIntegerField()
     eps_ok = models.BooleanField(null=True)
@@ -255,5 +270,9 @@ class FinancialResultWatch(models.Model):
     y_over_y_growth_rate = models.FloatField()
     note_url = models.URLField(null=True)
     symbol = models.ForeignKey(Symbol, on_delete=models.SET_NULL, null=True)
-    eps_unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name='r_eps_unit')
-    sales_unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name='r_sales_unit')
+    eps_unit = models.ForeignKey(
+        Unit, on_delete=models.CASCADE, related_name="r_eps_unit"
+    )
+    sales_unit = models.ForeignKey(
+        Unit, on_delete=models.CASCADE, related_name="r_sales_unit"
+    )
