472175d87a8bfa4c45114bd5932e157e9bfc99a0
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-18 11:07:31 +0900

    fix: Sourceのtypeを識別する

--- linebot_engine/domain/valueobject/line.py
+++ linebot_engine/domain/valueobject/line.py
@@ -5,7 +5,26 @@ class WebhookEvent:
 
     class Source:
         def __init__(self, source_dict):
+            self.type = source_dict.get("type")
             self.user_id = source_dict.get("userId")
+            self.group_id = source_dict.get("groupId")
+            self.room_id = source_dict.get("roomId")
+
+        def is_user(self):
+            return self.type == "user"
+
+        def is_group(self):
+            """
+            Notes: 3人以上でトークを利用することを「グループトーク」という
+            """
+            return self.type == "group"
+
+        def is_room(self):
+            """
+            Notes: 1対1のトーク中に友だちを誘って会話することを「複数人トーク」という
+                   ※現在は、グループトークに機能が統合されているらしい
+            """
+            return self.type == "room"
 
     def __init__(self, event: dict):
         self.type = event.get("type")
