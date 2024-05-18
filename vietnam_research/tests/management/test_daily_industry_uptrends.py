99348d5da21f1e4c5b1253b71f8e921389b83483
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-18 22:08:23 +0900

    fix: formatter

--- vietnam_research/tests/management/test_daily_industry_uptrends.py
+++ vietnam_research/tests/management/test_daily_industry_uptrends.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
 import pandas as pd
+from django.test import TestCase
 
-from vietnam_research.management.commands.daily_industry_chart_and_uptrends import calc_price, formatted_text
+from vietnam_research.management.commands.daily_industry_chart_and_uptrend import (
+    calc_price,
+    formatted_text,
+)
 
 
 class Test(TestCase):
@@ -11,17 +14,36 @@ class Test(TestCase):
     """
 
     def test_calc_price(self):
-        data = pd.Series([7.17, 7.14, 7.07, 7.07, 7.19, 7.19, 7.12, 7.16, 7.40, 7.58, 7.59, 7.79, 8.33, 8.22])
+        data = pd.Series(
+            [
+                7.17,
+                7.14,
+                7.07,
+                7.07,
+                7.19,
+                7.19,
+                7.12,
+                7.16,
+                7.40,
+                7.58,
+                7.59,
+                7.79,
+                8.33,
+                8.22,
+            ]
+        )
         expected = 1.05
-        self.assertEqual(calc_price(data)['delta'], expected)
+        self.assertEqual(calc_price(data)["delta"], expected)
 
     def test_formatted_text_has_value(self):
         code = "AAA"
         slopes = [-0.123, 0.25, -0.1]
         passed = 1
         price = {"initial": 100, "latest": 150, "delta": 50}
-        expected = f"{code}｜{slopes}, passed: {passed}, initial: {price.get('initial')}," \
-                   f" latest: {price.get('latest')}, delta: {price.get('delta')}"
+        expected = (
+            f"{code}｜{slopes}, passed: {passed}, initial: {price.get('initial')},"
+            f" latest: {price.get('latest')}, delta: {price.get('delta')}"
+        )
         self.assertEqual(formatted_text(code, slopes, passed, price), expected)
 
     def test_formatted_text_has_no_value(self):
@@ -29,5 +51,7 @@ class Test(TestCase):
         slopes = [-0.123, 0.25, -0.1]
         passed = 1
         price = {}
-        expected = f"{code}｜{slopes}, passed: {passed}, initial: -, latest: -, delta: -"
+        expected = (
+            f"{code}｜{slopes}, passed: {passed}, initial: -, latest: -, delta: -"
+        )
         self.assertEqual(formatted_text(code, slopes, passed, price), expected)
