d359b74d1b842cd2a096da1b1950d720ddca482f
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-25 10:28:49 +0900

    fix: create template

--- securities/urls.py
+++ securities/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from securities.views import IndexView
 
-app_name = "report"
+app_name = "securities"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
 ]
