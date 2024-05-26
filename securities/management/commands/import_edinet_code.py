7cd1315123ace594e2b680f748add71cabd0b0a9
yoshi <yoshitakaOkada0214@gmail.com>
2024-05-26 11:53:13 +0900

    nanがnullになるようになった

--- securities/management/commands/import_edinet_code.py
+++ securities/management/commands/import_edinet_code.py
@@ -6,6 +6,10 @@ from django.core.management.base import BaseCommand
 from securities.models import Edinet
 
 
+def na(value):
+    return value if pd.notna(value) else None
+
+
 class Command(BaseCommand):
     help = "Import edinet code upload from CSV"
 
@@ -15,7 +19,6 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        # TODO: 決算日は 5月31日 のように入っている
         folder_path = options["folder_path"]
         filename = "EdinetcodeDlInfo.csv"
         file_path = Path(folder_path) / filename
@@ -24,7 +27,7 @@ class Command(BaseCommand):
             return
         Edinet.objects.all().delete()
 
-        # Note: 最初の行には `ダウンロード実行日,2024年05月25日現在,件数,10827件` のようなメタデータが入っている
+        # Note: 最初の行には `ダウンロード実行日...` のようなメタデータが入っているのでskip
         df = pd.read_csv(
             file_path,
             skiprows=1,
@@ -35,32 +38,29 @@ class Command(BaseCommand):
                 "証券コード": str,
                 "提出者法人番号": str,
             },
-        )  # Skip the meta row
-        df["資本金"] = (
-            df["資本金"].fillna(0).astype(str).str.replace(",", "").astype(float)
         )
-        df["連結の有無"] = df["連結の有無"].fillna("").replace("", "無")
-
         # 3行目以降のデータを保存
+        edinet_list = []
         for index, row in df.iterrows():
-            Edinet.objects.create(
-                edinet_code=row["ＥＤＩＮＥＴコード"],
-                type_of_submitter=row["提出者種別"],
-                listing_status=row["上場区分"],
-                consolidated_status=row["連結の有無"],
-                capital=row["資本金"],
-                end_fiscal_year=row["決算日"],
-                submitter_name=row["提出者名"],
-                submitter_name_en=row["提出者名（英字）"],
-                submitter_name_kana=row["提出者名（ヨミ）"],
-                address=row["所在地"],
-                submitter_industry=row["提出者業種"],
-                securities_code=row["証券コード"],
-                corporate_number=row["提出者法人番号"],
+            edinet_list.append(
+                Edinet(
+                    edinet_code=na(row["ＥＤＩＮＥＴコード"]),
+                    type_of_submitter=na(row["提出者種別"]),
+                    listing_status=na(row["上場区分"]),
+                    consolidated_status=na(row["連結の有無"]),
+                    capital=(int(row["資本金"]) if pd.notna(row["資本金"]) else None),
+                    end_fiscal_year=na(row["決算日"]),
+                    submitter_name=na(row["提出者名"]),
+                    submitter_name_en=na(row["提出者名（英字）"]),
+                    submitter_name_kana=na(row["提出者名（ヨミ）"]),
+                    address=na(row["所在地"]),
+                    submitter_industry=na(row["提出者業種"]),
+                    securities_code=na(row["証券コード"]),
+                    corporate_number=na(row["提出者法人番号"]),
+                )
             )
+        Edinet.objects.bulk_create(edinet_list)
 
         self.stdout.write(
-            self.style.SUCCESS(
-                "Successfully imported all edinet master from XBRL files."
-            )
+            self.style.SUCCESS("Successfully imported all edinet code from CSV")
         )
--- securities/models.py
+++ securities/models.py
@@ -5,16 +5,18 @@ class Edinet(models.Model):
     class Meta:
         db_table = "securities_m_edinet"
 
-    edinet_code = models.CharField("ＥＤＩＮＥＴコード", max_length=6)
-    type_of_submitter = models.CharField("提出者種別", max_length=30)
-    listing_status = models.CharField("上場区分", max_length=3)
-    consolidated_status = models.CharField("連結の有無", max_length=1)
-    capital = models.FloatField("資本金")
-    end_fiscal_year = models.CharField("決算日", max_length=6)
-    submitter_name = models.CharField("提出者名", max_length=100)
-    submitter_name_en = models.CharField("提出者名（英字）", max_length=100)
-    submitter_name_kana = models.CharField("提出者名（ヨミ）", max_length=100)
-    address = models.CharField("所在地", max_length=255)
-    submitter_industry = models.CharField("提出者業種", max_length=25)
-    securities_code = models.CharField("証券コード", max_length=5)
-    corporate_number = models.CharField("提出者法人番号", max_length=13)
+    edinet_code = models.CharField("ＥＤＩＮＥＴコード", max_length=6, null=True)
+    type_of_submitter = models.CharField("提出者種別", max_length=30, null=True)
+    listing_status = models.CharField("上場区分", max_length=3, null=True)
+    consolidated_status = models.CharField("連結の有無", max_length=1, null=True)
+    capital = models.IntegerField("資本金", null=True)
+    end_fiscal_year = models.CharField("決算日", max_length=6, null=True)
+    submitter_name = models.CharField("提出者名", max_length=100, null=True)
+    submitter_name_en = models.CharField("提出者名（英字）", max_length=100, null=True)
+    submitter_name_kana = models.CharField(
+        "提出者名（ヨミ）", max_length=100, null=True
+    )
+    address = models.CharField("所在地", max_length=255, null=True)
+    submitter_industry = models.CharField("提出者業種", max_length=25, null=True)
+    securities_code = models.CharField("証券コード", max_length=5, null=True)
+    corporate_number = models.CharField("提出者法人番号", max_length=13, null=True)
