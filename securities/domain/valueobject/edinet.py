4c798b88f31771a36d643efdbfb0e1ad72ab4684
yoshi <yoshitakaOkada0214@gmail.com>
2024-06-01 10:01:31 +0900

    日付チェックを実装

--- securities/domain/valueobject/edinet.py
+++ securities/domain/valueobject/edinet.py
@@ -9,6 +9,13 @@ class RequestData:
     end_date: datetime.date
 
     def __post_init__(self):
+        if self.start_date > datetime.date.today():
+            raise ValueError("start_date is in the future")
+        if self.end_date > datetime.date.today():
+            raise ValueError("end_date is in the future")
+        if self.start_date > self.end_date:
+            raise ValueError("start_date is later than end_date")
+
         self.doc_type = self.SECURITIES_REPORT_AND_META_DATA
 
         # Calculate day_list
