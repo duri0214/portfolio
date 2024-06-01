c81913f701bede39097b83dbb7bbe219385eebc5
yoshi <yoshitakaOkada0214@gmail.com>
2024-06-01 17:57:56 +0900

    位置変えた

--- securities/models.py
+++ securities/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 
 class Edinet(models.Model):
-    class Meta:
-        db_table = "securities_m_edinet"
-
     edinet_code = models.CharField("ＥＤＩＮＥＴコード", max_length=6, null=True)
     type_of_submitter = models.CharField("提出者種別", max_length=30, null=True)
     listing_status = models.CharField("上場区分", max_length=3, null=True)
@@ -20,3 +17,6 @@ class Edinet(models.Model):
     submitter_industry = models.CharField("提出者業種", max_length=25, null=True)
     securities_code = models.CharField("証券コード", max_length=5, null=True)
     corporate_number = models.CharField("提出者法人番号", max_length=13, null=True)
+
+    class Meta:
+        db_table = "securities_m_edinet"
