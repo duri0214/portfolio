6222a4991e8cb4c5927a7a85d018f570234e48eb
yoshi <yoshitakaOkada0214@gmail.com>
2024-07-20 01:13:32 +0900

    編集ページのリファクタリング

--- shopping/urls.py
+++ shopping/urls.py
@@ -1,16 +1,28 @@
-"""urls.py"""
 from django.urls import path
-from .views import IndexView, ProductDetailView, UploadSingleView, UploadBulkView, StaffDetailView, StaffEditView, \
-    StaffCreateView
 
-app_name = 'shp'
+from .views import (
+    Index,
+    ProductDetail,
+    UploadSingle,
+    UploadBulk,
+    StaffDetail,
+    StaffEdit,
+    StaffCreate,
+    ProductEdit,
+)
+
+app_name = "shp"
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('product/edit/<int:mode>/', IndexView.as_view(), name='product_edit'),
-    path('product/detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('product/regist/single/', UploadSingleView.as_view(), name='product_register_single'),
-    path('product/regist/bulk/', UploadBulkView.as_view(), name='product_register_bulk'),
-    path('staff/detail/<int:pk>/', StaffDetailView.as_view(), name='staff_detail'),
-    path('staff/edit/<int:pk>/', StaffEditView.as_view(), name='staff_edit'),
-    path('staff/create/', StaffCreateView.as_view(), name='staff_create'),
+    path("", Index.as_view(), name="index"),
+    path("product/edit/<int:pk>/", ProductEdit.as_view(), name="product_edit"),
+    path("product/detail/<int:pk>/", ProductDetail.as_view(), name="product_detail"),
+    path(
+        "product/regist/single/",
+        UploadSingle.as_view(),
+        name="product_register_single",
+    ),
+    path("product/regist/bulk/", UploadBulk.as_view(), name="product_register_bulk"),
+    path("staff/detail/<int:pk>/", StaffDetail.as_view(), name="staff_detail"),
+    path("staff/edit/<int:pk>/", StaffEdit.as_view(), name="staff_edit"),
+    path("staff/create/", StaffCreate.as_view(), name="staff_create"),
 ]
