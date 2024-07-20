61f1fdc3e0f42303a21dfa27ccb201071049ff99
yoshi <yoshitakaOkada0214@gmail.com>
2024-07-20 21:58:03 +0900

    TESTを復活

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
+        "TEST": {
+            "NAME": "test_portfolio_db",
+        },
     }
 }
 
