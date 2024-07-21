1aca5e4247e0d545815b9d3a12312bf3af0f0307
yoshi <yoshitakaOkada0214@gmail.com>
2024-07-21 22:50:38 +0900

    filterをなくしてチャンク対応版のbulk insertに変更

--- vietnam_research/management/commands/fao_food_balance_chart.py
+++ vietnam_research/management/commands/fao_food_balance_chart.py
@@ -25,12 +25,6 @@ class Command(BaseCommand):
             with z.open("FoodBalanceSheets_E_Asia_NOFLAG.csv") as f:
                 df = pd.read_csv(f, encoding="latin1").fillna(0)
 
-        # 水産物の供給量を抽出
-        df_filtered = df[
-            (df["Item"] == "Fish, Seafood")
-            & (df["Element"] == "Food supply quantity (kg/capita/yr)")
-        ]
-
         # 'Y2022' to 2022
         end_year_string = df.columns[-1]
         end_year = int(re.findall(r"\d+", end_year_string)[0])
@@ -39,7 +33,7 @@ class Command(BaseCommand):
         fao_food_balance_rankers: list[FaoFoodBalanceRankers] = []
         for year in range(2010, end_year + 1):
             year_column = f"Y{year}"
-            df_sorted = df_filtered.sort_values(year_column, ascending=False)
+            df_sorted = df.sort_values(year_column, ascending=False)
             for i, (_, row) in enumerate(df_sorted.iterrows()):
                 fao_food_balance_rankers.append(
                     FaoFoodBalanceRankers(
@@ -54,9 +48,17 @@ class Command(BaseCommand):
                 )
 
         # bulk-insert
-        log_service = LogService("./result.log")
-        FaoFoodBalanceRankers.objects.bulk_create(fao_food_balance_rankers)
+        chunk_size = 100000
+        for i in range(0, len(fao_food_balance_rankers), chunk_size):
+            print(
+                f"Processing chunk {i//chunk_size + 1}/{len(fao_food_balance_rankers) // chunk_size}"
+            )
+            FaoFoodBalanceRankers.objects.bulk_create(
+                fao_food_balance_rankers[i : i + chunk_size]
+            )
+
         caller_file_name = Path(__file__).stem
+        log_service = LogService("./result.log")
         log_service.write(
             f"{caller_file_name} is done.({len(fao_food_balance_rankers)})"
         )
