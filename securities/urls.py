18aad4b221323b0480bfc9e4c34ee48644accf4c
yoshi <yoshitakaOkada0214@gmail.com>
2024-08-16 22:10:56 +0900

    add goto table_view

--- securities/urls.py
+++ securities/urls.py
@@ -5,12 +5,14 @@ from securities.views import (
     EdinetCodeUploadView,
     EdinetCodeUploadSuccessView,
     DownloadReserveView,
+    CountingView,
 )
 
 app_name = "securities"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("download_reserve/", DownloadReserveView.as_view(), name="download_reserve"),
+    path("table_view/counting", CountingView.as_view(), name="counting"),
     path(
         "edinet_code_upload/upload",
         EdinetCodeUploadView.as_view(),
