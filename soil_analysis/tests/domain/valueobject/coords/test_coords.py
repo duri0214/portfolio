159ff8a3fbeed03ba307e870a580a8d991146e66
yoshi <yoshitakaOkada0214@gmail.com>
2024-08-24 16:59:25 +0900

    formatter

--- soil_analysis/tests/domain/valueobject/coords/test_coords.py
+++ soil_analysis/tests/domain/valueobject/coords/test_coords.py
@@ -12,7 +12,7 @@ class TestCoords(TestCase):
         self.assertEqual((137.6490462, 34.7442398), xarvio_coords.get_coords())
         self.assertEqual("137.6490462, 34.7442398", xarvio_coords.get_coords(to_str=True))
 
-    def test_land_coords_to_googlemapcoords(self):
+    def test_land_coords_to_googlemap_coords(self):
         coords_str = "137.6489657,34.7443565 137.6491266,34.744123"
         land_coords = LandCoords(coords_str)
         googlemap_coords = land_coords.to_googlemapcoords()
@@ -24,7 +24,7 @@ class TestCoords(TestCase):
         self.assertEqual((34.7443565, 137.6489657), googlemap_coords.get_coords())
         self.assertEqual("34.7443565, 137.6489657", googlemap_coords.get_coords(to_str=True))
 
-    def test_capturelocation_coord_get_coords(self):
-        capturelocation_coords = CaptureLocationCoords(34.7443565, 137.6489657)
-        self.assertEqual((34.7443565, 137.6489657), capturelocation_coords.get_coords())
-        self.assertEqual("34.7443565, 137.6489657", capturelocation_coords.get_coords(to_str=True))
+    def test_capture_location_coord_get_coords(self):
+        capture_location_coords = CaptureLocationCoords(34.7443565, 137.6489657)
+        self.assertEqual((34.7443565, 137.6489657), capture_location_coords.get_coords())
+        self.assertEqual("34.7443565, 137.6489657", capture_location_coords.get_coords(to_str=True))
