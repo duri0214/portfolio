2bfafdafb9b9b389c95bdf6cb2d2eb5a7e0a2e71
yoshi <yoshitakaOkada0214@gmail.com>
2024-08-27 21:47:04 +0900

    vo追加

--- soil_analysis/domain/valueobject/weather/weather.py
+++ soil_analysis/domain/valueobject/weather/weather.py
@@ -17,3 +17,35 @@ class JmaConst:
     name: str
     children: list[str]
     parent: str
+
+
+@dataclass
+class RegionWeather:
+    """
+    weather information for a specific region.
+
+    Attributes:
+      region_code (str): The code of the region.
+      region_name (str): The name of the region.
+      weather_code (str): The code representing the weather in the region.
+    """
+
+    region_code: str
+    region_name: str
+    weather_code: str
+
+
+@dataclass
+class Amedas:
+    """
+    Amedas temperature data.
+
+    Attributes:
+        code (str): The code associated with the Amedas station.
+        min_temps (float): The minimum temperature data.
+        max_temps (float): The maximum temperature data.
+    """
+
+    code: str
+    min_temps: float
+    max_temps: float
