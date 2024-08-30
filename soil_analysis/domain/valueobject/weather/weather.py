09fc85e984a3ba631ce878d1633682957b3afad0
yoshi <yoshitakaOkada0214@gmail.com>
2024-08-30 21:07:34 +0900

    練ったvoを追加

--- soil_analysis/domain/valueobject/weather/weather.py
+++ soil_analysis/domain/valueobject/weather/weather.py
@@ -46,38 +46,115 @@ class JmaConst:
 
 
 # 予報データ
-@dataclass
+class MeanCalculable:
+    """
+    平均を出すための生値とその平均値
+
+    Args:
+        float_list (list[float]): 生値
+    """
+
+    def __init__(self, float_list: list[float]) -> None:
+        self.values: list[float] = float_list
+        self.mean: float = sum(float_list) / len(float_list) if float_list else 0
+
+
+@dataclass(frozen=True)
 class Region:
     """
     A class representing a region.
 
     Attributes:
-        code (str): The code of the region.
-        name (str): The name of the region.
+        code (str): "280010"
+        name (str): "南部"
     """
 
     code: str
     name: str
 
 
-@dataclass
-class SummaryData:
+@dataclass(frozen=True)
+class SummaryText:
     """
-    気象情報: [0]のサマリー情報
+    気象情報: サマリー情報
+
+    Attributes:
+        weather (str): "雨　所により　雷を伴い　非常に　激しく　降る"
+        wind (str): "東の風　やや強く　海上　では　東の風　強く"
+        wave (str): "１．５メートル　ただし　淡路島南部　では　３メートル　うねり　を伴う"
+    """
+
+    weather: str
+    wind: str
+    wave: str
+
+
+@dataclass(frozen=True)
+class RainData:
+    """
+    気象情報: 降水確率
+
+    Attributes:
+        values (MeanCalculable): 生値とその平均値（時間帯平均）
+        unit (str): "%"
+    """
+
+    values: MeanCalculable
+    unit: str = "%"
+
+
+@dataclass(frozen=True)
+class TemperatureData:
+    """
+    気象情報: 気温
+
+    Attributes:
+        min_values (MeanCalculable): 生値とその平均値（region平均）
+        max_values (MeanCalculable): 生値とその平均値（region平均）
+        unit (str): "℃"
+    """
+
+    min_values: MeanCalculable
+    max_values: MeanCalculable
+    unit: str = "℃"
+
+
+@dataclass(frozen=True)
+class WindData:
+    """
+    気象情報: 最大風速
+
+    Attributes:
+        values (MeanCalculable): 生値とその平均値（時間帯平均）
+        unit (str): "以下" or "メートル毎秒"
+    """
+
+    values: MeanCalculable
+    unit: str
+
+
+@dataclass(frozen=True)
+class WeatherData:
+    """
+    気象情報: 1日分の天気情報
+
+    Attributes:
 
     Attributes:
         time_defined (datetime): "2024-08-29T17:00:00+09:00"
         code (str): "300"
-        weather_summary (str): "雨　所により　雷を伴い　非常に　激しく　降る"
-        wind_summary (str): "東の風　やや強く　海上　では　東の風　強く"
-        wave_summary (str): "１．５メートル　ただし　淡路島南部　では　３メートル　うねり　を伴う"
+        summary_text (SummaryText): "雨　所により　雷を伴い　非常に　激しく　降る"
+        rain_data (RainData): "60%"
+        temperature_data (TemperatureData): "25℃"
+        wind_data (WindData): 9以下
     """
 
     time_defined: datetime
     code: str
-    weather_summary: str
-    wind_summary: str
-    wave_summary: str
+    summary_text: SummaryText
+    rain_data: RainData
+    temperature_data: TemperatureData
+    wind_data: WindData
 
 
 @dataclass
