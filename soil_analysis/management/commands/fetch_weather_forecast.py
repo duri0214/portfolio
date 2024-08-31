ea4b3e049f1bd59a5d389da80594f1938dba860e
yoshi <yoshitakaOkada0214@gmail.com>
2024-08-31 13:06:05 +0900

    いったんコメントアウト

--- soil_analysis/management/commands/fetch_weather_forecast.py
+++ soil_analysis/management/commands/fetch_weather_forecast.py
@@ -4,16 +4,11 @@ import requests
 from django.core.management import BaseCommand
 
 from soil_analysis.domain.valueobject.weather.jma import (
-    Region,
     WeatherForecast,
-    WeatherData,
-    SummaryText,
-    RainData,
-    TemperatureData,
     WindData,
     MeanCalculable,
 )
-from soil_analysis.models import JmaWeather, JmaAmedas
+from soil_analysis.models import JmaWeather
 
 THREE_DAYS = 0
 
@@ -164,13 +159,62 @@ class Command(BaseCommand):
                     )
                     weather_data_list.append(weather_data)
 
-                # ここまでOK
-
-                # その地域にあるアメダスid
-                amedas_ids = [
-                    amedas.id
-                    for amedas in JmaAmedas.objects.filter(jma_area3_id=region.code)
-                ]
+            # 天気コード・天気サマリ・風サマリ・波サマリ（いまは tomorrow のみ）
+            # url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{prefecture_id}.json"
+            # time_series_overview_data, indexes = get_data_and_indexes(
+            #     url=url, desired_date=tomorrow
+            # )
+            # time_defines = time_series_overview_data[TYPE_OVERVIEW]["timeDefines"]
+            # for region_data in time_series_overview_data[TYPE_OVERVIEW]["areas"]:
+            #     # Create Region instance
+            #     region = Region(
+            #         code=region_data["area"]["code"],
+            #         name=region_data["area"]["name"],
+            #     )
+            #     # Create list of Weather instances
+            #     weather_data_list: list[WeatherData] = []
+            #     for (
+            #         time_define,
+            #         weather_code,
+            #         weather_text,
+            #         wind_text,
+            #         wave_text,
+            #     ) in zip(
+            #         time_defines,
+            #         region_data["weatherCodes"],
+            #         region_data["weathers"],
+            #         region_data["winds"],
+            #         region_data["waves"],
+            #     ):
+            #         rain_data = forecasts_time_series[TYPE_RAIN]
+            #         temperature_data = forecasts_time_series[TYPE_TEMPERATURE]
+            #         weather_data = WeatherData(
+            #             time_defined=datetime.fromisoformat(time_define),
+            #             code=weather_code,
+            #             summary_text=SummaryText(
+            #                 weather=weather_text, wind=wind_text, wave=wave_text
+            #             ),
+            #             rain_data=RainData(),
+            #             temperature_data=TemperatureData(),
+            #             wind_data=WindData(),
+            #         )
+            #         weather_data_list.append(weather_data)
+            #
+            #     # ここまでOK
+            #
+            #     # その地域にあるアメダスid
+            #     amedas_ids = [
+            #         amedas.id
+            #         for amedas in JmaAmedas.objects.filter(jma_area3_id=region.code)
+            #     ]
+            #
+            # # 降水確率（いまは tomorrow のみ）
+            # for region_data in time_series_overview_data[TYPE_RAIN]["areas"]:
+            #     continue
+            #
+            # # 気温（いまは tomorrow のみ）
+            # for region_data in time_series_overview_data[TYPE_TEMPERATURE]["areas"]:
+            #     continue
 
             # # 天気・風・波・降水確率・気温 をガッチャンコ
             weather_forecast_list: list[WeatherForecast] = []
