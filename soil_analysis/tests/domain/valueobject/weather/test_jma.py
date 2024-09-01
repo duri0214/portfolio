fdb74196e8ae5e7c2b9c79b75c2427bc85cf5a70
yoshi <yoshitakaOkada0214@gmail.com>
2024-09-01 17:52:19 +0900

    test ok

--- soil_analysis/tests/domain/valueobject/weather/test_jma.py
+++ soil_analysis/tests/domain/valueobject/weather/test_jma.py
@@ -1,41 +1,13 @@
-from datetime import date, datetime
+from datetime import datetime
 from unittest import mock
 
 from django.test import TestCase
 
-from soil_analysis.domain.valueobject.weather.jma import XXXTemperatureData
 from soil_analysis.management.commands import fetch_weather_forecast
 from soil_analysis.management.commands.fetch_weather_forecast import (
     get_data,
     get_indexes,
 )
-
-
-class TestGetIndexFromTimeDefines(TestCase):
-    def test_get_indexes_from_time_defines(self):
-        time_defines = [
-            "2023-03-17T00:00:00+09:00",
-            "2023-03-18T00:00:00+09:00",
-            "2023-03-18T09:00:00+09:00",
-            "2023-03-19T00:00:00+09:00",
-            "2023-03-19T09:00:00+09:00",
-        ]
-        target_date = date(2023, 3, 18)
-
-        min_temps_idx, max_temps_idx = XXXTemperatureData.get_indexes_from_time_defines(
-            time_defines, target_date
-        )
-        self.assertEqual(min_temps_idx, 1)
-        self.assertEqual(max_temps_idx, 2)
-
-    def test_no_indexes_in_time_defines(self):
-        time_defines = ["2023-03-16", "2023-03-17", "2023-03-19", "2023-03-20"]
-        target_date = date(2023, 3, 18)
-
-        indexes = XXXTemperatureData.get_indexes_from_time_defines(
-            time_defines, target_date
-        )
-        self.assertEqual(indexes, [])
 
 
 class TestFetchWeatherForecast(TestCase):
