9220a929fe275c30bd3e20b2eb97000755fdbc70
yoshi <yoshitakaOkada0214@gmail.com>
2024-09-23 15:45:34 +0900

    fix: 市区名マスタ

--- hospital/models.py
+++ hospital/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class WardType(models.Model):
-    name = models.CharField(verbose_name="病棟種", max_length=100)
+    name = models.CharField(verbose_name="病棟種", max_length=100, unique=True)
 
 
 class Ward(models.Model):
@@ -13,5 +13,16 @@ class Ward(models.Model):
     name = models.CharField(
         verbose_name="病棟名", max_length=100, null=False, blank=False
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+class City(models.Model):
+    name = models.CharField(verbose_name="エリア", max_length=100, unique=True)
+
+
+class CitySector(models.Model):
+    name = models.CharField(verbose_name="市区", max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
