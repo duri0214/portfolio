25e705f67cef4f5343d8cdcbdf35b6ecbca75164
yoshi <yoshitakaOkada0214@gmail.com>
2024-09-28 15:24:00 +0900

    fix: postsの原始表示とページ詳細の原始表示

--- home/views.py
+++ home/views.py
@@ -1,5 +1,17 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from home.models import Post
 
 
 class IndexView(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all()
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "home/post/detail.html"
