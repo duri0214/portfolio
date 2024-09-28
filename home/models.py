57e77f081d73d5e8e6d9377a075873b517fdf28d
yoshi <yoshitakaOkada0214@gmail.com>
2024-09-29 06:08:07 +0900

    fix: update page OK

--- home/models.py
+++ home/models.py
@@ -1,13 +1,25 @@
+import os
+import uuid
+
 from django.db import models
+
+
+def get_random_filename(instance, filename):
+    ext = filename.split(".")[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("home/posts/", filename)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="home/posts/", blank=True, null=True)
+    image = models.ImageField(upload_to=get_random_filename, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     summary = models.TextField(blank=True)
     content = models.TextField()
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, DetailView, UpdateView
 from markdown import Markdown
 from mdx_gfm import GithubFlavoredMarkdownExtension
 
@@ -25,3 +26,12 @@ class PostDetailView(DetailView):
         context["content_html"] = md.convert(self.object.content)
 
         return context
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "home/posts/update.html"
+    fields = ["title", "image", "category", "summary", "content"]
+
+    def get_success_url(self):
+        return reverse_lazy("home:post_detail", kwargs={"pk": self.object.pk})
