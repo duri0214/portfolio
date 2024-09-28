1b09fc5e80c8af56e65cbc056017d09b89a67740
yoshi <yoshitakaOkada0214@gmail.com>
2024-09-28 14:55:44 +0900

    add models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="posts/", blank=True, null=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    summary = models.TextField(blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
