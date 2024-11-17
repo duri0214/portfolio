7788f860973661fa58647929567058d03c040b9e
yoshi <yoshitakaOkada0214@gmail.com>
2024-11-17 17:31:59 +0900

    Refactor coordinate classes and method documentation.

--- soil_analysis/domain/valueobject/coords.py
+++ soil_analysis/domain/valueobject/coords.py
@@ -2,37 +2,40 @@ from abc import ABC, abstractmethod
 
 
 class BaseCoords(ABC):
-    """
-    座標を表すベースクラス。緯度と経度を持つ。
-
-    メソッド:
-    get_coords : 座標を取得します。戻り値の形式を指定することもできます。
-
-    抽象メソッド:
-    get_coords
-    """
-
     def __init__(self, latitude: float, longitude: float):
-        """
-        BaseCoords クラスのインスタンスを生成します。
-
-        パラメータ:
-        latitude (float): 緯度
-        longitude (float): 経度
-        """
         self.latitude = latitude
         self.longitude = longitude
 
     @abstractmethod
     def to_tuple(self) -> tuple[float, float]:
+        """
+        座標をタプル形式で取得します。緯度経度の順に配置されます。
+
+        Returns:
+            tuple[float, float]: (Latitude, Longitude)
+        """
         pass
 
     @abstractmethod
     def to_str(self) -> str:
+        """
+        座標を文字列形式で取得します。緯度経度はコンマで区切られます。
+
+        Returns:
+            str: "Latitude, Longitude"
+        """
         pass
 
 
 class GoogleMapCoords(BaseCoords):
+    """
+    Google Map 用の座標変換クラス。BaseCoordsを継承します。
+
+    メソッド:
+    to_tuple: 座標をタプル形式で取得します。戻り値は (緯度,経度) 法です。
+    to_str : 座標を文字列形式で取得します。戻り値は "緯度, 経度" 形式です。
+    """
+
     def to_tuple(self) -> tuple[float, float]:
         return self.latitude, self.longitude
 
@@ -41,6 +44,17 @@ class GoogleMapCoords(BaseCoords):
 
 
 class CaptureLocationCoords(BaseCoords):
+    """
+    Capture Location 用の座標変換クラス。BaseCoordsを継承します。
+    xarvio用に作ったので、順序が Google Map とは異なり、(経度,緯度)です。
+    ※写真側の緯度経度取り扱い形式にしないといけないのかもしれない
+
+    メソッド:
+    to_tuple: 座標をタプル形式で取得します。戻り値は (経度,緯度) 形式です。
+    to_str : 座標を文字列形式で取得します。戻り値は "経度, 緯度" 形式です。
+    to_googlemap: CaptureLocation 用の座標を GoogleMapCoords に変換します。
+    """
+
     def to_tuple(self) -> tuple[float, float]:
         return self.longitude, self.latitude
 
@@ -54,7 +68,20 @@ class CaptureLocationCoords(BaseCoords):
 class LandCoords(BaseCoords):
     def __init__(self, coords_str: str):
         """
-        xarvioは圃場情報を 経度緯度(lng, lat) のタプルを4以上で構成し、その4以上の座標をspaceで区切ってエクスポートする
+        Land 用の座標変換クラス。BaseCoordsを継承します。
+        xarvio用に作ったので、順序が Google Map とは異なり、(経度,緯度)です。
+
+        メソッド:
+        to_tuple: 座標をタプル形式で取得します。戻り値は (経度,緯度) 形式です。
+        to_str : 座標を文字列形式で取得します。戻り値は "経度, 緯度" 形式です。
+        to_googlemap: Land 用の座標を GoogleMapCoords に変換します。
+
+        Notes:
+        xarvioは圃場情報を 経度緯度(lng, lat) のタプルで4以上（通常5）で構成し、その座標をスペースで区切ってエクスポートします。たとえば：
+        <coordinates>137.6489657,34.7443565 137.6491266,34.744123 137.648613,34.7438929
+        137.6484413,34.7441175 137.6489657,34.7443565</coordinates>
+        上記の座標は1つの圃場を表し、最初と最後の座標は同じ位置を示しています（縮小ループ）。
+
         See Also: https://developers.google.com/kml/documentation/kmlreference?hl=ja#coordinates
         """
         super().__init__(0.0, 0.0)  # initial values set to zero
