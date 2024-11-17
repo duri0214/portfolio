7c6b5265a5f5aaff50b7fe721c7accc2495338d2
yoshi <yoshitakaOkada0214@gmail.com>
2024-11-17 13:58:21 +0900

    Refactor coordinate initialization and calculations.

--- soil_analysis/domain/valueobject/coords.py
+++ soil_analysis/domain/valueobject/coords.py
@@ -57,17 +57,21 @@ class LandCoords(BaseCoords):
         xarvioは圃場情報を 経度緯度(lng, lat) のタプルを4以上で構成し、その4以上の座標をspaceで区切ってエクスポートする
         See Also: https://developers.google.com/kml/documentation/kmlreference?hl=ja#coordinates
         """
+        super().__init__(0.0, 0.0)  # initial values set to zero
+
         coords = coords_str.split()
         coords = list(set(coords))  # 始点と終点の座標が一致するため、重複を排除する
-        self.latitude_sum = 0.0
-        self.longitude_sum = 0.0
-        self.num_points = len(coords)
+        latitude_sum = 0.0
+        longitude_sum = 0.0
+        num_points = len(coords)
+
         for coord in coords:
             lng, lat = coord.split(",")
-            self.longitude_sum += float(lng)
-            self.latitude_sum += float(lat)
-        self.longitude = round(self.longitude_sum / self.num_points, 7)
-        self.latitude = round(self.latitude_sum / self.num_points, 7)
+            longitude_sum += float(lng)
+            latitude_sum += float(lat)
+
+        self.longitude = round(longitude_sum / num_points, 7)
+        self.latitude = round(latitude_sum / num_points, 7)
 
     def to_tuple(self) -> tuple[float, float]:
         return self.longitude, self.latitude
