c43df4ef50ad24bcd6b84aa761d427a253222c13
yoshi <yoshitakaOkada0214@gmail.com>
2024-11-30 22:48:49 +0900

    Fix 不在者投票事務処理簿

--- hospital/domain/service/export_report.py
+++ hospital/domain/service/export_report.py
@@ -9,7 +9,10 @@ from django.http import HttpResponse
 from openpyxl.reader.excel import load_workbook
 
 from config.settings import BASE_DIR
-from hospital.domain.valueobject.export_report import BillingListRow
+from hospital.domain.valueobject.export_report import (
+    BillingListRow,
+    VotingManagementListRow,
+)
 from hospital.models import ElectionLedger
 
 
@@ -70,7 +73,102 @@ class ExportBillingService(AbstractExport):
                 excel_data,
                 content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
             )
-            header_content = f"attachment; filename*=UTF-8''{quote("請求者名簿.xlsx")}"
+            header_content = f"attachment; filename*=UTF-8''{quote('請求者名簿.xlsx')}"
+            response["Content-Disposition"] = header_content
+        finally:
+            os.remove(self.temp_folder / filename)
+
+        return response
+
+
+class VotingManagementService(AbstractExport):
+    def __init__(self, temp_folder: Path):
+        super().__init__(temp_folder, chunk_size=11, start_row=13)
+
+    def export(self, election_id) -> HttpResponse:
+        # TODO: 元帳ではなく投票管理に関連するデータを取得します（出力済みのものは印刷しない、モードつける？）
+        ledgers = (
+            ElectionLedger.objects.filter(election_id=election_id)
+            .select_related("vote_ward")
+            .order_by("vote_ward__name")
+        )
+
+        wb = load_workbook(
+            BASE_DIR / "hospital/domain/service/xlsx/voting_management_list.xlsx"
+        )
+        filename = self.create_unique_filename()
+
+        for ward_name, group in groupby(ledgers, key=lambda x: x.vote_ward.name):
+            sheet_counter = 1
+            ledgers_iter = iter(group)
+            while True:
+                chunk = list(islice(ledgers_iter, self.chunk_size))
+                if not chunk:
+                    break
+
+                new_worksheet = wb.copy_worksheet(wb["ひな形"])
+                new_worksheet.title = (
+                    ward_name
+                    if sheet_counter == 1
+                    else f"{ward_name} ({sheet_counter})"
+                )
+                sheet_counter += 1
+
+                for i, ledger in enumerate(chunk, start=0):
+                    row = VotingManagementListRow(ledger)
+                    current_row_index = self.start_row + i * 2
+                    new_worksheet.cell(
+                        row=current_row_index, column=2, value=str(i + 1)
+                    )  # B列
+                    new_worksheet.cell(
+                        row=current_row_index, column=3, value=row.address
+                    )  # C列
+                    new_worksheet.cell(
+                        row=current_row_index, column=4, value=row.voter_name
+                    )  # D列
+                    new_worksheet.cell(
+                        row=current_row_index, column=5, value=row.billing_method
+                    )  # E列
+                    new_worksheet.cell(
+                        row=current_row_index,
+                        column=6,
+                        value=row.proxy_billing_request_date,
+                    )  # F列
+                    new_worksheet.cell(
+                        row=current_row_index, column=7, value=row.proxy_billing_date
+                    )  # G列
+                    new_worksheet.cell(
+                        row=current_row_index, column=8, value=row.ballot_received_date
+                    )  # H列
+                    new_worksheet.cell(
+                        row=current_row_index, column=9, value=row.vote_date
+                    )  # I列
+                    new_worksheet.cell(
+                        row=current_row_index, column=10, value=row.vote_place
+                    )  # J列
+                    new_worksheet.cell(
+                        row=current_row_index, column=11, value=row.voter_witness
+                    )  # K列
+                    new_worksheet.cell(
+                        row=current_row_index,
+                        column=12,
+                        value=row.applied_for_proxy_voting,
+                    )  # L列
+                    new_worksheet.cell(
+                        row=current_row_index, column=15, value=row.delivery_date
+                    )  # O列
+        del wb["ひな形"]
+        wb.save(filename)
+
+        try:
+            excel_data = self.get_excel_data(filename)
+            response = HttpResponse(
+                excel_data,
+                content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            )
+            header_content = (
+                f"attachment; filename*=UTF-8''{quote('不在者投票事務処理簿.xlsx')}"
+            )
             response["Content-Disposition"] = header_content
         finally:
             os.remove(self.temp_folder / filename)
--- hospital/domain/valueobject/export_report.py
+++ hospital/domain/valueobject/export_report.py
@@ -57,3 +57,107 @@ class BillingListRow(AbstractRow):
 
     def to_list(self) -> list:
         return [self.address, self.voter_name, self.date_of_birth, self.ward_name]
+
+
+@dataclass
+class VotingManagementListRow(AbstractRow):
+    ledger: ElectionLedger
+
+    @property
+    def address(self) -> str:
+        return self.ledger.voter.userattribute.address
+
+    @property
+    def voter_name(self) -> str:
+        return self.ledger.voter.username
+
+    @property
+    def billing_method(self) -> str:
+        return self.ledger.get_billing_method_display()
+
+    @property
+    def proxy_billing_request_date(self) -> str:
+        if self.ledger.proxy_billing_request_date is None:
+            return "XX.XX"
+        else:
+            return self.ledger.proxy_billing_request_date.strftime("%m.%d")
+
+    @property
+    def proxy_billing_date(self) -> str:
+        if self.ledger.proxy_billing_date is None:
+            return "XX.XX"
+        else:
+            return self.ledger.proxy_billing_date.strftime("%m.%d")
+
+    @property
+    def ballot_received_date(self) -> str:
+        if self.ledger.ballot_received_date is None:
+            return "XX.XX"
+        else:
+            return self.ledger.ballot_received_date.strftime("%m.%d")
+
+    @property
+    def vote_date(self) -> str:
+        if self.ledger.vote_date is None:
+            return "XX.XX"
+        else:
+            return self.ledger.vote_date.strftime("%m.%d")
+
+    @property
+    def vote_place(self) -> str:
+        if self.ledger.vote_place is None:
+            return "（未記入）"
+        else:
+            return self.ledger.vote_place.name
+
+    @property
+    def voter_witness(self) -> str:
+        if self.ledger.vote_observer is None:
+            return "（未記入）"
+        else:
+            return self.ledger.vote_observer.username
+
+    @property
+    def applied_for_proxy_voting(self) -> str:
+        if self.ledger.applied_for_proxy_voting:
+            return "有"
+        else:
+            return "無"
+
+    @property
+    def delivery_date(self) -> str:
+        if self.ledger.delivery_date is None:
+            return "XX.XX"
+        else:
+            return self.ledger.delivery_date.strftime("%m.%d")
+
+    @staticmethod
+    def get_field_names() -> list[str]:
+        return [
+            "選挙人住所",
+            "選挙人氏名",
+            "投票用紙の請求方法",
+            "代理請求依頼日",
+            "代理請求日",
+            "投票用紙受領日",
+            "投票日",
+            "投票場所",
+            "投票立会人",
+            "代理投票申請の有無",
+            "投票用紙送付日",
+        ]
+
+    def to_list(self) -> list:
+        return [
+            self.address,
+            self.voter_name,
+            self.billing_method,
+            self.proxy_billing_request_date,
+            self.proxy_billing_date,
+            self.ballot_received_date,
+            self.vote_date,
+            self.vote_place,
+            self.voter_witness,
+            self.applied_for_proxy_voting,
+            self.delivery_date,
+        ]
