889beced2c5d71ecdebf2032ac5a45083481c184
yoshi <yoshitakaOkada0214@gmail.com>
2024-12-03 23:21:40 +0900

    Add MarketDataRow class and tests for HTML parsing.

--- vietnam_research/domain/valueobject/vietkabu.py
+++ vietnam_research/domain/valueobject/vietkabu.py
@@ -67,3 +67,46 @@ class TransactionDate:
 
         date_str = f"{extracted.group()}:00"
         return datetime.strptime(date_str, "%Y/%m/%d %H:%M:%S")
+
+
+@dataclass
+class MarketDataRow:
+    code: str
+    name: str
+    industry1: str
+    industry2: str
+    open_price: float
+    high_price: float
+    low_price: float
+    closing_price: float
+    volume: float
+    marketcap: float
+    per: float
+
+    def __init__(self, tr: Tag):
+        super().__init__()
+        tag_tds_center = tr.find_all("td", class_="table_list_center")
+        company = Company(
+            code=re.sub("＊", "", tag_tds_center[0].text.strip()),
+            name=tag_tds_center[0].a.get("title"),
+            industry1=re.sub(r"\[(.+)]", "", tag_tds_center[1].img.get("title")),
+            industry2=re.search(
+                r"(?<=\[).*?(?=])", tag_tds_center[1].img.get("title")
+            ).group(),
+        )
+        self.code = company.code
+        self.name = company.name
+        self.industry1 = company.industry1
+        self.industry2 = company.industry2
+
+        tag_tds_right = [
+            Counting(td.text.strip())
+            for td in tr.find_all("td", class_="table_list_right")
+        ]
+        self.open_price = tag_tds_right[2].value
+        self.high_price = tag_tds_right[3].value
+        self.low_price = tag_tds_right[4].value
+        self.closing_price = tag_tds_right[1].value
+        self.volume = tag_tds_right[7].value
+        self.marketcap = tag_tds_right[9].value
+        self.per = tag_tds_right[10].value
--- vietnam_research/tests/management/test_daily_import_from_vietkabu.py
+++ vietnam_research/tests/management/test_daily_import_from_vietkabu.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 from django.test import TestCase
 
-from vietnam_research.domain.valueobject.vietkabu import Counting
+from vietnam_research.domain.valueobject.vietkabu import Counting, MarketDataRow
 from vietnam_research.management.commands.daily_import_from_vietkabu import (
     TransactionDate,
 )
@@ -72,3 +72,55 @@ class TestTransactionDate(TestCase):
                 TransactionDate(self.invalid_th_tag).to_date(),
                 datetime(2019, 8, 16, 17, 0),
             )
+
+
+class TestMarketDataRow(TestCase):
+    def test_market_data_row_from_html(self):
+        html = """
+        <tr bgcolor="#FFFFFF" id="APG" onmouseover="on_over('APG','#DDEFFF')" onmouseout="on_out('APG','#FFFFFF')" style="background: rgb(255, 255, 255);">
+            <td width="65" class="table_list_center">
+                <a title="APG証券" href="/hcm/APG.html">
+                APG     </a>
+            </td>
+            <td width="55" nowrap="" class="table_list_right">
+                <b>8.99</b>
+            </td>
+            <td width="55" nowrap="" class="table_list_right">
+                <b>9.04</b>
+            </td>
+            <td width="55" nowrap="" class="table_list_right">
+                <b>8.88</b>
+            </td>
+            <td width="55" nowrap="" class="table_list_right">
+                <b>9.04</b>
+            </td>
+            <td width="55" nowrap="" class="table_list_right">
+                <b>8.8</b>
+            </td>   	         
+            <td width="62" nowrap="" class="table_list_right"><span style="color:#006600">+50</span></td>
+            <td width="65" nowrap="" class="table_list_right"><span style="color:#006600">+0.56%</span></td>
+            <td width="62" nowrap="" class="table_list_right">304,300</td>
+            <td width="110" nowrap="" class="table_list_right">2,021,542</td>
+            <td width="88" nowrap="" class="table_list_right">128.76</td>
+            <td width="52" nowrap="" class="table_list_right">11.11</td>
+            <td width="75" nowrap="" class="table_list_right">0</td>
+            <td width="75" nowrap="" class="table_list_right">0</td>
+            <td width="84" nowrap="" class="table_list_center">
+               <img title="金融業[証券業]" src="/images/stock/fnc.gif" width="28" height="13" style="margin:0 4px;"> </td>
+        </tr>
+        """
+        soup = BeautifulSoup(html, "html.parser")
+        tr = soup.find("tr")
+        row = MarketDataRow(tr)
+        # These values are checked using the html snippet above.
+        self.assertEqual(row.code, "APG")
+        self.assertEqual(row.name, "APG証券")
+        self.assertEqual(row.industry1, "金融業")
+        self.assertEqual(row.industry2, "証券業")
+        self.assertEqual(row.open_price, 8.88)
+        self.assertEqual(row.high_price, 9.04)
+        self.assertEqual(row.low_price, 8.8)
+        self.assertEqual(row.closing_price, 9.04)
+        self.assertEqual(row.volume, 304300)
+        self.assertEqual(row.marketcap, 128.76)
+        self.assertEqual(row.per, 11.11)
