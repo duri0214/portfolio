4662944c827566e751486bc6b3234e8e85cbd287
yoshi <yoshitakaOkada0214@gmail.com>
2024-12-14 16:06:41 +0900

    fix: RoleTypeに変更

--- llm_chat/models.py
+++ llm_chat/models.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from lib.llm.valueobject.chat import RoleType
+
 
 class ChatLogsWithLine(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=255)
+    role = models.CharField(
+        max_length=9,
+        choices=[(x.name, x.value) for x in RoleType],
+    )
     content = models.TextField()
     file_path = models.CharField(max_length=255, null=True)
     invisible = models.BooleanField(default=False)
