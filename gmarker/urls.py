cec85115b249ba2790bd94b94229f56ab982c70a
yoshi <yoshitakaOkada0214@gmail.com>
2024-12-24 23:10:08 +0900

    Update URL patterns and integrate `SearchDetailView`
    Replaced the function-based `search_detail` view with the class-based `SearchDetailView` for improved clarity and reusability. Made minor formatting adjustments to align with style consistency.

--- gmarker/urls.py
+++ gmarker/urls.py
@@ -1,11 +1,14 @@
-"""urls.py"""
 from django.urls import path
-from . import views
 
-app_name = 'mrk'
+from . import views
+from .views import SearchDetailView
+
+app_name = "mrk"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('search/<str:search_code>', views.index, name='index_search'),
-    path('search/detail/<str:place_id>', views.search_detail, name='detail_search'),
-    path('result/<str:search_code>', views.index, name='index_result'),
+    path("", views.index, name="index"),
+    path("search/<str:search_code>", views.index, name="index_search"),
+    path(
+        "search/detail/<str:place_id>", SearchDetailView.as_view(), name="detail_search"
+    ),
+    path("result/<str:search_code>", views.index, name="index_result"),
 ]
