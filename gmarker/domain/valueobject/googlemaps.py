c99cf6c1f250e9cb83c1c02387c2d5ce4c2626a9
yoshi <yoshitakaOkada0214@gmail.com>
2024-12-24 23:10:59 +0900

    Add Google Maps value object classes
    Created `PlaceVO`, `PlacePhoto`, and `PlaceDetail` classes to represent data retrieved from the Google Places API. These include properties for place details, photos, and coordinates to simplify handling of API responses.

---
+++ gmarker/domain/valueobject/googlemaps.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+from lib.geo.valueobject.coords import GoogleMapCoords
+
+
+@dataclass
+class PlacePhoto:
+    height: int
+    width: int
+    html_attributions: list[str]
+    photo_reference: str
+
+
+@dataclass
+class PlaceDetail:
+    pass
+
+
+@dataclass
+class PlaceVO:
+    """
+    このデータクラスは、Google Places APIから取得した場所の詳細情報を表します。
+
+    プロパティ:
+        search_word: 場所の検索ワード
+        place_id: 場所のGoogle Places IDを表す文字列
+        name: 場所の名前を表す文字列です。
+        location: 場所の座標
+        photos: 場所の写真のリスト
+        reviews: 場所のレビューを表す文字列のリスト
+        is_status_ok: APIからのレスポンスにエラーがなかった場合にTrue
+    """
+
+    search_word: str
+    place_id: str
+    name: str
+    location: GoogleMapCoords
+    photos: list[PlacePhoto]
+    detail: PlaceDetail | None
