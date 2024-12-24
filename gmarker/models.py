db8220acd43dbc3db594249c572e835f4d17e3ba
yoshi <yoshitakaOkada0214@gmail.com>
2024-12-24 23:09:22 +0900

    Refactor models for better clarity and data organization.
    Renamed and restructured models: `StoreInformation` to `NearbyPlace` and `SignageMenuName` to `CategorySearchMaster`. Updated field names, added detailed docstrings, and introduced choice fields for improved readability and maintainability.

--- gmarker/models.py
+++ gmarker/models.py
@@ -1,22 +1,57 @@
 from django.db import models
 
 
-class StoreInformation(models.Model):
+class NearbyPlace(models.Model):
     """
-    StoreInformation
-    search_code9は自拠点
+    NearbyPlaceモデルはGoogle Placesから取得した近隣の場所に関する情報を保存します。
+
+    code9は自拠点を示します。
+    カテゴリーフィールドは以下の値を持ます:
+        1 = "Category": 具体的なカテゴリーで検索され、登録された場所
+        2 = "Pin Select": Googleマップ上でピンを選んで登録した場所
+        3 = "Database Insert": データベースから直接挿入された情報
+        9 = "Default Location": デフォルト位置（マップを初期表示したときの中心）
+
+    主にユーザーが画面から登録するのは "Category"と"Pin Select"です。 "Database Insert"は主にメンテナンスのときに使います。
     """
-    category = models.IntegerField()
-    searchword = models.CharField(null=True, blank=True, max_length=100)
+
+    CATEGORY_SEARCH = 1
+    PIN_SELECT = 2
+    DATABASE_INSERT = 3
+    DEFAULT_LOCATION = 9
+
+    # TODO: このフィールドは「必要」なのか？
+    category = models.IntegerField(
+        choices=[
+            (CATEGORY_SEARCH, "Category"),
+            (PIN_SELECT, "Pin Select"),
+            (DATABASE_INSERT, "Database Insert"),
+            (DEFAULT_LOCATION, "Default Location"),
+        ]
+    )
+    search_word = models.CharField(null=True, blank=True, max_length=100)
     place_id = models.CharField(null=True, blank=True, max_length=200)
-    shop_name = models.CharField(max_length=200)
-    shop_latlng = models.CharField(max_length=100)
-    create_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
 
-class SignageMenuName(models.Model):
+class CategorySearchMaster(models.Model):
     """
-    SignageName
+    CategorySearchMaster is a model that serves as a Master table for category search.
+
+    Fields:
+        code (CharField): The unique code associated with each category.
+        name (CharField): The name of the category associated with each unique code.
+
+    Sample data:
+        "1A", "ラーメン"
+        "1B", "洋食"
+        "1C", "和食"
+
+    This model is used to fetch the category name using the code.
     """
-    menu_code = models.CharField(primary_key=True, max_length=2)
-    menu_name = models.CharField(max_length=100)
+
+    code = models.CharField(primary_key=True, max_length=2)
+    name = models.CharField(max_length=100)
