c4d0600365950fc351b3a5021c304a5f392ca71b
yoshi <yoshitakaOkada0214@gmail.com>
2025-01-05 19:17:20 +0900

    Add Celery settings to configuration
    This change introduces `CELERY_BROKER_URL` and `CELERY_RESULT_BACKEND` in the settings file, pulling values from environment variables. These settings are essential for integrating Celery into the application for managing asynchronous tasks.

--- config/settings.py
+++ config/settings.py
@@ -172,3 +172,7 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
+# Celery関連の設定
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
