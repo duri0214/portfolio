fe2cf4a9aac5dac4eaa26ff9d859d459f10bbb7e
yoshi <yoshitakaOkada0214@gmail.com>
2025-01-19 23:29:24 +0900

    Add URL pattern for coordinate creation view
    Introduced a new route, `coords/create/`, mapped to `CoordinateRegisterView`. This allows handling coordinate creation functionality in the application. Updated the `urlpatterns` to include this new endpoint.

--- gmarker/urls.py
+++ gmarker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from gmarker.views import IndexView
+from gmarker.views import IndexView, CoordinateRegisterView
 
 app_name = "mrk"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("coords/create/", CoordinateRegisterView.as_view(), name="coordinate_create"),
     path("search/<int:search_code>", IndexView.as_view(), name="nearby_search"),
 ]
