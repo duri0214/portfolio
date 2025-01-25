0f855f53c770c4bc386a7dd89e9ef7a2703e8be5
yoshi <yoshitakaOkada0214@gmail.com>
2025-01-25 20:16:29 +0900

    Add ReviewVO and update PlaceVO structure
    Introduced the ReviewVO dataclass to represent detailed review information obtained from the Google Places API. Updated the PlaceVO dataclass to include a `place` attribute tied to the Place model and replaced the `reviews` list type with a list of ReviewVO instances for more structured review data.

--- gmarker/domain/valueobject/googlemaps.py
+++ gmarker/domain/valueobject/googlemaps.py
@@ -1,6 +1,26 @@
 from dataclasses import dataclass
 
+from gmarker.models import Place
 from lib.geo.valueobject.coords import GoogleMapCoords
+
+
+@dataclass
+class ReviewVO:
+    """
+    このデータクラスは、Google Places APIから取得するレビューの詳細情報を表します。
+
+    Attributes:
+        text: レビューのテキストを表現する文字列。
+        author: レビューの投稿者の名前を表す文字列。
+        publish_time: レビューの公開時間を表す文字列。
+        google_maps_uri: レビューに関連するGoogleマップのURIを表す文字列。
+
+    """
+
+    text: str
+    author: str
+    publish_time: str
+    google_maps_uri: str
 
 
 @dataclass
@@ -8,16 +28,16 @@ class PlaceVO:
     """
     このデータクラスは、Google Places APIから取得した場所の詳細情報を表します。
 
-    プロパティ:
-        place_id: 場所のGoogle Places IDを表す文字列。
+    Attributes:
+        place: このレビューが関連付けられるPlaceモデルのインスタンス。
         location: 場所の座標を表すGoogleMapCoordsのインスタンス。
         name: 場所の名前を表す文字列。
         rating: 場所の評価を表す浮動小数点数。
         reviews: 将来的に場所のレビュー情報を含む可能性があるリスト。
     """
 
-    place_id: str
+    place: Place
     location: GoogleMapCoords
     name: str
     rating: float
-    reviews: list
+    reviews: list[ReviewVO]
