a353f6006f47ae697a63f79b1b998efd1ec4eaf9
yoshi <yoshitakaOkada0214@gmail.com>
2025-01-25 15:46:37 +0900

    Fix EMAIL_HOST_PASSWORD environment variable reference
    Replaced incorrect reference to "REGISTER" with "EMAIL_HOST_PASSWORD" in `settings.py`. Updated `.env.example` to remove unused "REGISTER" variable and include "EMAIL_HOST_PASSWORD" for clarity.

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("REGISTER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
