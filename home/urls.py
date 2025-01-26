cec2ee7b03587f116866debef1049748fc6433d6
yoshi <yoshitakaOkada0214@gmail.com>
2025-01-26 22:57:16 +0900

    Remove post-related views and dependencies from the app
    Deleted PostDetailView, PostUpdateView, and PostCreateView, along with their respective URLs. Also removed Markdown and GitHub Flavored Markdown dependencies from requirements.txt. This simplifies the app by removing unused post management functionality.

--- home/urls.py
+++ home/urls.py
@@ -2,17 +2,11 @@ from django.urls import path
 
 from home.views import (
     IndexView,
-    PostDetailView,
-    PostUpdateView,
-    PostCreateView,
     HospitalIndexView,
 )
 
 app_name = "home"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
-    path("post/create/", PostCreateView.as_view(), name="post_create"),
     path("hospital/", HospitalIndexView.as_view(), name="hospital_index"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,54 +1,8 @@
-from django.urls import reverse_lazy, reverse
-from django.views.generic import TemplateView, DetailView, UpdateView, CreateView
-from markdown import Markdown
-from mdx_gfm import GithubFlavoredMarkdownExtension
-
-from home.models import Post
+from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = "home/index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["posts"] = Post.objects.all()
-        context["featured_posts"] = Post.objects.filter(is_featured=True)
-        return context
-
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "home/post/detail.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        md = Markdown(extensions=[GithubFlavoredMarkdownExtension()])
-        context["content_html"] = md.convert(self.object.content)
-
-        return context
-
-
-class PostUpdateView(UpdateView):
-    model = Post
-    template_name = "home/post/update.html"
-    fields = ["title", "image", "category", "summary", "content", "is_featured"]
-
-    def get_success_url(self):
-        return reverse_lazy("home:post_detail", kwargs={"pk": self.object.pk})
-
-
-class PostCreateView(CreateView):
-    model = Post
-    template_name = "home/post/create.html"
-    fields = ["title", "image", "category", "summary", "content", "is_featured"]
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse("home:post_detail", args=[str(self.object.id)])
 
 
 class HospitalIndexView(TemplateView):
