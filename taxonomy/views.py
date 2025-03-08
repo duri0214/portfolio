1fceb991e8575004f5b0ff5f3d47bd5a3013ce83
yoshi <yoshitakaOkada0214@gmail.com>
2025-03-08 22:53:11 +0900

    Refactor Breed hierarchy retrieval into repository class
    Moved Breed hierarchy query from the IndexView to a new BreedRepository class to improve separation of concerns and reusability. This change simplifies the view logic and centralizes the query logic in a dedicated repository.

--- taxonomy/views.py
+++ taxonomy/views.py
@@ -1,11 +1,12 @@
 import json
-from django.db.models import F
+
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
 
 from taxonomy.domain.breed_entity import BreedEntity
 from taxonomy.domain.node import NodeTree
-from taxonomy.models import Kingdom, Breed
+from taxonomy.domain.repository.breed import BreedRepository
+from taxonomy.models import Kingdom
 
 
 class IndexView(ListView):
@@ -13,27 +14,7 @@ class IndexView(ListView):
     template_name = "taxonomy/index.html"
 
     def get_queryset(self):
-        return Breed.objects \
-            .annotate(
-            kingdom_name=F('species__genus__family__classification__phylum__kingdom__name'),
-            phylum_name=F('species__genus__family__classification__phylum__name'),
-            classification_name=F('species__genus__family__classification__name'),
-            family_name=F('species__genus__family__name'),
-            genus_name=F('species__genus__name'),
-            species_name=F('species__name'),
-            breed_name=F('name'),
-            breed_name_kana=F('name_kana'),
-            natural_monument_name=F('natural_monument__name'),
-            breed_tag=F('breedtags__tag__name')
-        ) \
-            .values(
-            'kingdom_name', 'phylum_name', 'classification_name', 'family_name', 'genus_name', 'species_name',
-            'breed_name', 'breed_name_kana', 'natural_monument_name', 'breed_tag'
-        ) \
-            .order_by(
-            'kingdom_name', 'phylum_name', 'classification_name', 'family_name', 'genus_name', 'species_name',
-            'breed_name', 'breed_name_kana', 'natural_monument_name', 'breed_tag'
-        )
+        return BreedRepository.get_breed_hierarchy()
 
     def get_context_data(self, *, object_list=None, **kwargs):
         """
@@ -41,7 +22,7 @@ class IndexView(ListView):
         """
         context = super(IndexView, self).get_context_data(**kwargs)
         tree = NodeTree([BreedEntity(record) for record in self.get_queryset()])
-        context['data'] = json.dumps(tree.export(), ensure_ascii=False)
+        context["data"] = json.dumps(tree.export(), ensure_ascii=False)
 
         return context
 
