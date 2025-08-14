f2b8aa5d913416c1ce130280146f8f931403a9ae
yoshi <yoshitakaOkada0214@gmail.com>
2025-08-14 21:39:11 +0900

    fix: refactor test setup to create and associate Place objects with NearbyPlace

--- gmarker/tests/test_views.py
+++ gmarker/tests/test_views.py
@@ -1,29 +1,41 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from gmarker.models import NearbyPlace
+from gmarker.models import NearbyPlace, Place
 
 
 class TestView(TestCase):
     @classmethod
     def setUpTestData(cls):
-        NearbyPlace.objects.create(
-            category=9,
+        # まずPlaceオブジェクトを作成
+        place1 = Place.objects.create(
+            place_id="ChIJXSModoALGGARILWiCfeu2M0",
             name="渋谷駅",
             location="35.6598003,139.7023894",
-            created_at="2023-02-12 00:00:00",
         )
-        NearbyPlace.objects.create(
-            category=3,
+        place2 = Place.objects.create(
+            place_id="ChIJXSModoALGGARILWiCfeu2M1",
             name="忠犬ハチ公像",
             location="35.6590439,139.7005917",
-            created_at="2023-02-12 00:00:00",
+        )
+        place3 = Place.objects.create(
+            place_id="ChIJXSModoALGGARILWiCfeu2M2",
+            name="明治神宮",
+            location="35.676236,139.6993411",
+        )
+
+        # NearbyPlaceオブジェクトを作成し、Placeを関連付け
+        NearbyPlace.objects.create(
+            category=9,
+            place=place1,
         )
         NearbyPlace.objects.create(
             category=3,
-            name="明治神宮",
-            location="35.676236,139.6993411",
-            created_at="2023-02-12 00:00:00",
+            place=place2,
+        )
+        NearbyPlace.objects.create(
+            category=3,
+            place=place3,
         )
 
     def test_get_top_page_200(self):
