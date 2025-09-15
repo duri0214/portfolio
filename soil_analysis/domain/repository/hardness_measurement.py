e70d45cf67c567ecea9232317cde28be11a4ec74
yoshi <yoshitakaOkada0214@gmail.com>
2025-09-15 14:04:23 +0900

    refactor: remove unused soil hardness measurement repository methods and corresponding tests

--- soil_analysis/domain/repository/hardness_measurement.py
+++ soil_analysis/domain/repository/hardness_measurement.py
@@ -5,48 +5,6 @@ from soil_analysis.models import SoilHardnessMeasurement, Land, LandLedger
 
 class SoilHardnessMeasurementRepository:
     @staticmethod
-    def get_measurements_by_memory_range(
-        memory_anchor: int, total_sampling_times: int
-    ) -> QuerySet:
-        """
-        指定されたメモリアンカーからtotal_sampling_times分の計測データを取得します
-
-        Args:
-            memory_anchor: 開始メモリ位置
-            total_sampling_times: 計測データの総数
-
-        Returns:
-            QuerySet: SoilHardnessMeasurementのクエリセット
-        """
-        start_index = memory_anchor
-        end_index = memory_anchor + (total_sampling_times - 1)
-
-        return SoilHardnessMeasurement.objects.filter(
-            set_memory__range=(start_index, end_index)
-        ).order_by("pk")
-
-    @staticmethod
-    def group_measurements(queryset: QuerySet = None) -> QuerySet:
-        """
-        計測データをメモリセットごとにグループ化して取得します
-
-        Args:
-            queryset: 基となるクエリセット（指定がない場合は土地ブロックが割り当てられていない全てのSoilHardnessMeasurementを使用）
-
-        Returns:
-            QuerySet: メモリセットごとにグループ化された計測データ
-            folder(圃場)別、set_memory(メモリ番号)別にソート
-        """
-        if queryset is None:
-            queryset = SoilHardnessMeasurement.objects.filter(land_block__isnull=True)
-
-        return (
-            queryset.values("folder", "set_memory", "set_datetime")
-            .annotate(cnt=Count("pk"))
-            .order_by("folder", "set_memory")
-        )
-
-    @staticmethod
     def get_folder_stats(associated_only: bool = False) -> list:
         """
         フォルダ別の統計情報を取得します
