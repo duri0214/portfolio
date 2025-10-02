28f71794a01c3c6b363881475ca3486eb213541e
yoshi <yoshitakaOkada0214@gmail.com>
2025-10-02 21:21:59 +0900

    refactor(urls): remove unused company and land list URLs for cleanup and maintainability

--- soil_analysis/urls.py
+++ soil_analysis/urls.py
@@ -7,17 +7,11 @@ from . import views
 app_name = "soil"
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
-    path("company/list", views.CompanyListView.as_view(), name="company_list"),
     path("company/create", views.CompanyCreateView.as_view(), name="company_create"),
     path(
         "company/<int:pk>/detail",
         views.CompanyDetailView.as_view(),
         name="company_detail",
-    ),
-    path(
-        "company/<int:company_id>/land/list",
-        views.LandListView.as_view(),
-        name="land_list",
     ),
     path(
         "company/<int:company_id>/land/create",
