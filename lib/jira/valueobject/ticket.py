db7f00b0325d35b4e255d37cab8bb471483324c7
yoshi <yoshitakaOkada0214@gmail.com>
2025-10-09 22:38:18 +0900

    chore(valueobject): remove redundant punctuation in docstrings for improved readability

--- lib/jira/valueobject/ticket.py
+++ lib/jira/valueobject/ticket.py
@@ -4,11 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class ProjectVO:
     """
-    Value Object (VO) to represent a JIRA project.
+    Value Object (VO) to represent a JIRA project
 
     Attributes:
-        key (str): The unique key of the project.
-        name (str): The name of the project.
+        key (str): The unique key of the project
+        name (str): The name of the project
     """
 
     key: str
@@ -18,13 +18,13 @@ class ProjectVO:
 @dataclass
 class SubTaskVO:
     """
-    Data class to represent sub-tasks of an issue.
+    Data class to represent subtasks of an issue
 
     Attributes:
-        key (str): The unique key of the sub-task.
-        name (str): The name of the sub-task.
-        status (str): The current status of the sub-task.
-        priority (str): The priority of the sub-task.
+        key (str): The unique key of the subtask
+        name (str): The name of the subtask
+        status (str): The current status of the subtask
+        priority (str): The priority of the subtask
     """
 
     key: str
@@ -36,16 +36,16 @@ class SubTaskVO:
 @dataclass
 class IssueVO:
     """
-    Data class to represent an issue.
+    Data class to represent an issue
 
     Attributes:
-        key (str): The unique key of the issue.
-        name (str): The title or summary of the issue.
-        description (str): The detailed description of the issue.
-        priority (str): The priority of the issue.
-        assignee (str): The display name of the assigned user.
-        status (str): The current status of the issue.
-        sub_tasks (list[SubTaskVO]): The list of sub-tasks associated with the issue.
+        key (str): The unique key of the issue
+        name (str): The title or summary of the issue
+        description (str): The detailed description of the issue
+        priority (str): The priority of the issue
+        assignee (str): The display name of the assigned user
+        status (str): The current status of the issue
+        sub_tasks (list[SubTaskVO]): The list of subtasks associated with the issue
     """
 
     key: str
