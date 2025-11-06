f9a6b6a51fd1e0719288362b1d9280d155f9bad1
yoshi <yoshitakaOkada0214@gmail.com>
2025-11-06 20:09:55 +0900

    refactor(models/migrations): remove `created_at` and `updated_at` fields from taxonomy models

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -19,9 +19,6 @@ class JmaWeatherCode(models.Model):
     name = models.CharField(max_length=20)
     name_en = models.CharField(max_length=100)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True)
-
 
 class Kingdom(models.Model):
     """
