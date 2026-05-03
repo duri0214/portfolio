0b0b2534f0614b926c0e42c7c4c7fb55ac583224
yoshi <yoshitakaOkada0214@gmail.com>
2026-05-04 02:20:44 +0900

    fix: correct tooltip title retrieval logic in chemical.js

--- soil_analysis/static/soil_analysis/js/land_report/chemical.js
+++ soil_analysis/static/soil_analysis/js/land_report/chemical.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl, {
             title: function () {
-                var titleSelector = this.getAttribute('data-bs-title');
+                var titleSelector = tooltipTriggerEl.getAttribute('data-bs-title');
                 var titleTarget = document.querySelector(titleSelector);
                 return titleTarget ? titleTarget.innerHTML : "";
             }
