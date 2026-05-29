28ddc70205a89733eaca35efd11501cd22304072
yoshi <yoshitakaOkada0214@gmail.com>
2026-05-30 00:13:14 +0900

    feat: add `get_company_by_id` method to retrieve a company by its ID in `CompanyRepository`

--- soil_analysis/domain/repository/company.py
+++ soil_analysis/domain/repository/company.py
@@ -14,3 +14,16 @@ class CompanyRepository:
             list[Company]: 会社のリスト
         """
         return list(Company.objects.filter(category_id=category_id).order_by("name"))
+
+    @staticmethod
+    def get_company_by_id(company_id: int) -> Company:
+        """
+        IDを指定して会社を取得します
+
+        Args:
+            company_id: 会社ID
+
+        Returns:
+            Company: 会社
+        """
+        return Company.objects.get(pk=company_id)
